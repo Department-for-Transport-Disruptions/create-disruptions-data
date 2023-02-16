1aab12476ae922778b18c9574eed7bd3ecc1d433
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-02-16 11:00:43 +0000

    Added initial lambda

--- packages/siri-sx-generator/index.ts
+++ packages/siri-sx-generator/index.ts
@@ -1,6 +1,15 @@
-export const main = () => {
-    return {
-        statusCode: 200,
-        body: "Hello World",
-    };
+import { S3Event } from "aws-lambda";
+import * as AWS from "aws-sdk";
+
+export const main = async (event: S3Event): Promise<void> => {
+    const bucketName = event.Records[0].s3.bucket.name || "";
+    const objectKey = event.Records[0].s3.object.key || "";
+
+    const s3 = new AWS.S3();
+    const params = { Bucket: bucketName, Key: objectKey };
+    const response = await s3.getObject(params).promise();
+    const data = response.Body?.toString("utf-8") || "";
+
+    // eslint-disable-next-line no-console
+    console.log("file contents:", data);
 };
--- stacks/SiriGeneratorStack.ts
+++ stacks/SiriGeneratorStack.ts
@@ -1,18 +1,22 @@
-import { S3EventSource } from "aws-cdk-lib/aws-lambda-event-sources";
-import { Function, StackContext, use } from "sst/constructs";
+import { StackContext, use, Function } from "sst/constructs";
 import { SiteStack } from "./Site";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { EventType, Bucket } from "aws-cdk-lib/aws-s3";
+import { LambdaDestination } from "aws-cdk-lib/aws-s3-notifications";
 
 export function SiriGeneratorStack({ stack }: StackContext) {
     const { disruptionsJsonBucket } = use(SiteStack);
-
-    disruptionsJsonBucket.addNotifications(stack, {
-        triggerSiriGenerator: {
-            function: {
-                handler: "packages/siri-sx-generator/index.main",
-                timeout: 10,
-                memorySize: 256,
-            },
-            events: ["object_created"],
-        },
+    const siriGenerator = new Function(stack, "cdd-siri-generator", {
+        permissions: [
+            new PolicyStatement({
+                actions: ["s3:GetObject"],
+                resources: [`${disruptionsJsonBucket.bucketArn}/*`],
+            }),
+        ],
+        handler: "packages/siri-sx-generator/index.main",
+        timeout: 60,
+        memorySize: 256,
     });
+    const bucket = Bucket.fromBucketName(stack, "cdd-siri-generator-bucket", disruptionsJsonBucket.bucketName);
+    bucket.addEventNotification(EventType.OBJECT_CREATED, new LambdaDestination(siriGenerator));
 }
