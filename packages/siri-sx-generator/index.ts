069f03a7e9fa3e100fbb70f4f655b808e5437acb
Laurence Jones <laurence.jones@kpmg.co.uk>
2023-02-16 13:12:25 +0000

    [deps]: fixed dependency vulnerabilities

--- packages/siri-sx-generator/index.ts
+++ packages/siri-sx-generator/index.ts
@@ -1,14 +1,15 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { S3Event } from "aws-lambda";
-import { S3 } from "aws-sdk";
+
+const s3Client = new S3Client({ region: "eu-west-2" });
 
 export const main = async (event: S3Event): Promise<void> => {
     const bucketName = event.Records[0].s3.bucket.name || "";
     const objectKey = event.Records[0].s3.object.key || "";
 
-    const s3 = new S3();
     const params = { Bucket: bucketName, Key: objectKey };
-    const response = await s3.getObject(params).promise();
-    const data = response.Body?.toString("utf-8") || "";
+    const response = await s3Client.send(new GetObjectCommand(params));
+    const data = response.Body?.toString() || "";
 
     // eslint-disable-next-line no-console
     console.log("file contents:", data);
