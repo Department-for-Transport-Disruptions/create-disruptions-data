d489fabd804d0cde005e60d13027e43679f7e07c
alvak1 <karthik.alva@kpmg.co.uk>
2023-02-23 15:34:22 +0000

    Siri SX validator function to verify xml with schema

--- packages/siri-sx-generator/index.ts
+++ packages/siri-sx-generator/index.ts
@@ -2,6 +2,7 @@ import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { S3Event } from "aws-lambda";
 import { toXML } from "jstoxml";
 import { getS3Client, uploadToS3 } from "./util/s3Client";
+import xmlFormat from "xml-formatter";
 
 const s3Client = getS3Client();
 
@@ -11,7 +12,7 @@ export const main = async (event: S3Event): Promise<void> => {
 
     const params = { Bucket: bucketName, Key: objectKey };
     const response = await s3Client.send(new GetObjectCommand(params));
-    const data = await response.Body?.transformToString()
+    const data = await response.Body?.transformToString();
     const config = {
         indent: "    ",
     };
@@ -25,13 +26,9 @@ export const main = async (event: S3Event): Promise<void> => {
     if (!xmlData) {
         throw Error("Could not generate XML");
     } else {
-        xmlData = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-        <Siri version="2.0" xmlns="http://www.siri.org.uk/siri" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.siri.org.uk/siri http://www.siri.org.uk/schema/2.0/xsd/siri.xsd">
-            <ServiceDelivery srsName="">
-                    ${xmlData}
-                </ServiceDelivery>
-            </Siri>`;
+        xmlData = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Siri version="2.0" xmlns="http://www.siri.org.uk/siri" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.siri.org.uk/siri http://www.siri.org.uk/schema/2.0/xsd/siri.xsd">
+        <ServiceDelivery srsName="">${xmlData}</ServiceDelivery></Siri>`;
     }
 
-    await uploadToS3(xmlData, "siri-generated-xml.xml", process.env.SIRI_SX_UNVALIDATED_BUCKET_NAME)
+    await uploadToS3(xmlFormat(xmlData), "siri-generated-xml.xml", process.env.SIRI_SX_UNVALIDATED_BUCKET_NAME);
 };
