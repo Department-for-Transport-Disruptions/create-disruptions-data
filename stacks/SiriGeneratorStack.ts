3fdcdeea41651057f18e91f313f3e5ad21a26447
alvak1 <karthik.alva@kpmg.co.uk>
2023-02-27 10:40:28 +0000

    Includes changes for siri sx api

--- stacks/SiriGeneratorStack.ts
+++ stacks/SiriGeneratorStack.ts
@@ -1,8 +1,15 @@
-import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { PolicyStatement, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { BucketEncryption, EventType, Bucket as S3Bucket } from "aws-cdk-lib/aws-s3";
 import { LambdaDestination } from "aws-cdk-lib/aws-s3-notifications";
-import { Bucket, StackContext, use, Function } from "sst/constructs";
+import { Bucket, StackContext, use, Function, ApiGatewayV1Api } from "sst/constructs";
 import { SiteStack } from "./Site";
+import {
+    AuthorizationType,
+    AwsIntegration,
+    MethodOptions,
+    PassthroughBehavior,
+    Resource,
+} from "aws-cdk-lib/aws-apigateway";
 
 export function SiriGeneratorStack({ stack }: StackContext) {
     const { disruptionsJsonBucket } = use(SiteStack);
@@ -95,4 +102,88 @@ export function SiriGeneratorStack({ stack }: StackContext) {
         siriSXUnvalidatedBucket.bucketName,
     );
     validatorBucket.addEventNotification(EventType.OBJECT_CREATED, new LambdaDestination(siriValidator));
+
+    const apiGateway = new ApiGatewayV1Api(stack, "SiriSXApi", {
+        cdk: {
+            restApi: {
+                restApiName: "SiriSXApi",
+                description: "API to retrieve Siri SX XML data",
+            },
+        },
+    });
+
+    const bucketResource: Resource = apiGateway.cdk.restApi.root.addResource("{folder}");
+    const objectResource: Resource = bucketResource.addResource("{item}");
+    // apiGateway.cdk.restApi.root.addResource("{folder}").addMethod("GET", new AwsIntegration({service:'s3'}), {});
+
+    const executeRole = new Role(stack, "siri-sx-api-gateway-s3-assume-role", {
+        assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
+        roleName: "Siri-SX-API-Gateway-S3-Integration-Role",
+    });
+
+    executeRole.addToPolicy(
+        new PolicyStatement({
+            resources: [`${siriSXBucket.bucketArn}/*`],
+            actions: ["s3:GetObject"],
+        }),
+    );
+
+    const s3Integration: AwsIntegration = new AwsIntegration({
+        service: "s3",
+        region: "eu-west-2",
+        integrationHttpMethod: "GET",
+        path: "{bucket}/{object}",
+        options: {
+            credentialsRole: executeRole,
+            passthroughBehavior: PassthroughBehavior.WHEN_NO_MATCH,
+            requestParameters: {
+                // Specify the bucket name from the XML bucket we created above
+                "integration.request.path.bucket": "method.request.path.folder",
+                // Specify the object name using the APIG context requestId
+                "integration.request.path.object": "method.request.path.item",
+            },
+            integrationResponses: [
+                {
+                    statusCode: "200",
+                    responseParameters: {
+                        "method.response.header.Content-Type": "integration.response.header.Content-Type",
+                    },
+                },
+            ],
+        },
+    });
+
+    const s3ObjectMethod: MethodOptions = {
+        // Protected by API Key
+        authorizationType: AuthorizationType.NONE,
+        // Require the API Key on all requests
+        apiKeyRequired: true,
+        requestParameters: {
+            //"method.request.header.Content-Type": true,
+            "method.request.path.folder": true,
+            "method.request.path.item": true,
+        },
+        methodResponses: [
+            {
+                statusCode: "200",
+                responseParameters: {
+                    "method.response.header.Content-Type": true,
+                },
+            },
+        ],
+    };
+
+    objectResource.addMethod("GET", s3Integration, s3ObjectMethod);
+
+    const plan = apiGateway.cdk.restApi.addUsagePlan("UsagePlan", {
+        throttle: {
+            rateLimit: 50,
+            burstLimit: 10,
+        },
+        apiStages: [{ api: apiGateway.cdk.restApi, stage: apiGateway.cdk.restApi.deploymentStage }],
+    });
+
+    const key = apiGateway.cdk.restApi.addApiKey("ApiKey");
+
+    plan.addApiKey(key);
 }
