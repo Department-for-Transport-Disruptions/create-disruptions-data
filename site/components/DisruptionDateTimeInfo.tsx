172255df0862a7be290f06420875ead507bba994
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-03-01 14:45:32 +0000

    making ids unique

--- site/components/DisruptionDateTimeInfo.tsx
+++ site/components/DisruptionDateTimeInfo.tsx
@@ -12,11 +12,11 @@ interface DisruptionsDateTimeInfoProps {
 
 export const DisruptionsDateTimeInfo = ({ inputs, isDisruptionValidity }: DisruptionsDateTimeInfoProps): ReactElement => (
   <>
-    <fieldset className="govuk-fieldset" role="group" aria-describedby="start-date-hint">
+    <fieldset className="govuk-fieldset" role="group" aria-describedby={`${isDisruptionValidity ? 'validity' : 'publish'}-start-date-hint`}>
       <legend className="govuk-fieldset__legend govuk-!-padding-top-2">
         <h3 className="govuk-heading-s govuk-!-margin-bottom-0">What is the start date?</h3>
       </legend>
-      <div id="start-date-hint" className="govuk-hint">
+      <div id={`${isDisruptionValidity ? 'validity' : 'publish'}-start-date-hint`} className="govuk-hint">
         Enter in format DD/MM/YYYY
       </div>
       <DateSelector
@@ -27,13 +27,14 @@ export const DisruptionsDateTimeInfo = ({ inputs, isDisruptionValidity }: Disrup
           monthInput: isDisruptionValidity ? inputs.validityStartDateMonth : inputs.publishStartDateMonth,
           yearInput: isDisruptionValidity ? inputs.validityStartDateYear : inputs.publishStartDateYear,
         }}
+        type={isDisruptionValidity ? 'validity' : 'publish'}
       />
     </fieldset>
-    <fieldset className="govuk-fieldset" role="group" aria-describedby="start-time-hint">
+    <fieldset className="govuk-fieldset" role="group" aria-describedby={`${isDisruptionValidity ? 'validity' : 'publish'}-start-time-hint`}>
       <legend className="govuk-fieldset__legend govuk-!-padding-top-8">
         <h3 className="govuk-heading-s govuk-!-margin-bottom-0">What is the start time?</h3>
       </legend>
-      <div id="start-time-hint" className="govuk-hint" >
+      <div id={`${isDisruptionValidity ? 'validity' : 'publish'}-start-time-hint`} className="govuk-hint" >
         Enter in format HH:MM
       </div>
       <TimeSelector
@@ -43,13 +44,14 @@ export const DisruptionsDateTimeInfo = ({ inputs, isDisruptionValidity }: Disrup
           hourInput: isDisruptionValidity ? inputs.validityStartTimeHour : inputs.publishStartTimeHour,
           minuteInput: isDisruptionValidity ? inputs.validityStartTimeMinute : inputs.publishStartTimeMinute
         }}
+        type={isDisruptionValidity ? 'validity' : 'publish'}
       />
     </fieldset>
-    <fieldset className="govuk-fieldset" role="group" aria-describedby="end-date-hint">
+    <fieldset className="govuk-fieldset" role="group" aria-describedby={`${isDisruptionValidity ? 'validity' : 'publish'}-end-date-hint`}>
       <legend className="govuk-fieldset__legend govuk-!-padding-top-8">
         <h3 className="govuk-heading-s govuk-!-margin-bottom-0">What is the end date?</h3>
       </legend>
-      <div id="end-date-hint" className="govuk-hint" >
+      <div id={`${isDisruptionValidity ? 'validity' : 'publish'}-end-date-hint`} className="govuk-hint" >
         Enter in format DD/MM/YYYY
       </div>
       <DateSelector
@@ -60,13 +62,14 @@ export const DisruptionsDateTimeInfo = ({ inputs, isDisruptionValidity }: Disrup
           monthInput: isDisruptionValidity ? inputs.validityEndDateMonth : inputs.publishEndDateMonth,
           yearInput: isDisruptionValidity ? inputs.validityEndDateYear : inputs.publishEndDateYear,
         }}
+        type={isDisruptionValidity ? 'validity' : 'publish'}
       />
     </fieldset>
-    <fieldset className="govuk-fieldset" role="group" aria-describedby="end-time-hint">
+    <fieldset className="govuk-fieldset" role="group" aria-describedby={`${isDisruptionValidity ? 'validity' : 'publish'}-end-time-hint`}>
       <legend className="govuk-fieldset__legend govuk-!-padding-top-8">
         <h3 className="govuk-heading-s govuk-!-margin-bottom-0"> What is the end time?</h3>
       </legend>
-      <div id="end-time-hint" className="govuk-hint" >
+      <div id={`${isDisruptionValidity ? 'validity' : 'publish'}-end-time-hint`} className="govuk-hint" >
         Enter in format HH:MM
       </div>
       <TimeSelector
@@ -76,6 +79,7 @@ export const DisruptionsDateTimeInfo = ({ inputs, isDisruptionValidity }: Disrup
           hourInput: isDisruptionValidity ? inputs.validityEndTimeHour : inputs.publishEndTimeHour,
           minuteInput: isDisruptionValidity ? inputs.validityEndTimeMinute : inputs.publishEndTimeMinute
         }}
+        type={isDisruptionValidity ? 'validity' : 'publish'}
       />
     </fieldset>
     <fieldset className="govuk-fieldset" role="group">
