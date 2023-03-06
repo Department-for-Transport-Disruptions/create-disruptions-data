e86e94ec8cc4e2a324d3c2af138832968fba20a7
Laurence Jones <laurence.jones@kpmg.co.uk>
2023-03-06 22:21:58 +0000

    setup dns and deployment pipeline

--- stacks/SiriAPIStack.ts
+++ stacks/SiriAPIStack.ts
@@ -1,9 +1,11 @@
 import { StackContext, use } from "sst/constructs";
+import { DnsStack } from "./DnsStack";
 import { createSiriApi } from "./services/APIGateway";
 import { SiriGeneratorStack } from "./SiriGeneratorStack";
 
 export function SiriAPIStack({ stack }: StackContext) {
     const { siriSXBucket } = use(SiriGeneratorStack);
+    const { hostedZone } = use(DnsStack);
 
-    createSiriApi(stack, siriSXBucket);
+    createSiriApi(stack, siriSXBucket, hostedZone);
 }
--- stacks/SiteStack.ts
+++ stacks/SiteStack.ts
@@ -1,14 +1,22 @@
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { NextjsSite, StackContext, use } from "sst/constructs";
+import { DnsStack } from "./DnsStack";
 import { DynamoDBStack } from "./DynamoDBStack";
 
 export function SiteStack({ stack }: StackContext) {
     const { table } = use(DynamoDBStack);
+    const { hostedZone } = use(DnsStack);
+
+    const subDomain = ["test", "preprod", "prod"].includes(stack.stage) ? "" : `${stack.stage}`;
 
     const site = new NextjsSite(stack, "Site", {
         path: "site/",
         environment: {
             TABLE_ARN: table.tableArn,
+        },
+        customDomain: {
+            domainName: `${subDomain}.${hostedZone.zoneName}`,
+            hostedZone: hostedZone.zoneName,
         },
         permissions: [
             new PolicyStatement({
