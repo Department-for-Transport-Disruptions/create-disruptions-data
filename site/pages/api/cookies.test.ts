eacb48a353e9ff86b0c60049149093e5475d7402
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-03-06 17:04:26 +0000

    fix lint

--- site/pages/api/cookies.test.ts
+++ site/pages/api/cookies.test.ts
@@ -1,29 +1,29 @@
-import cookies from '../api/cookies'
-import * as apiUtils from '../../utils/apiUtils';
 import { describe, it, expect, afterEach, vi } from "vitest";
-import { getMockRequestAndResponse } from '../../pages/testData/mockData';
-import { COOKIES_POLICY_COOKIE, COOKIE_PREFERENCES_COOKIE, oneYearInSeconds } from '../../constants';
-import { CookiePolicy } from '../../interfaces';
+import { COOKIES_POLICY_COOKIE, COOKIE_PREFERENCES_COOKIE, oneYearInSeconds } from "../../constants";
+import { CookiePolicy } from "../../interfaces";
+import { getMockRequestAndResponse } from "../../pages/testData/mockData";
+import * as apiUtils from "../../utils/apiUtils";
+import cookies from "../api/cookies";
 
-describe('cookies', () => {
+describe("cookies", () => {
     const writeHeadMock = vi.fn();
-    const setCookieSpy = vi.spyOn(apiUtils, 'setCookieOnResponseObject');
+    const setCookieSpy = vi.spyOn(apiUtils, "setCookieOnResponseObject");
 
     afterEach(() => {
         vi.resetAllMocks();
     });
 
-    it('should redirect back to itself (i.e. /cookies) when no tracking selection is sent to the API', () => {
+    it("should redirect back to itself (i.e. /cookies) when no tracking selection is sent to the API", () => {
         const { req, res } = getMockRequestAndResponse({ body: {}, mockWriteHeadFn: writeHeadMock });
         cookies(req, res);
-        expect(writeHeadMock).toBeCalledWith(302, { Location: '/cookies' });
+        expect(writeHeadMock).toBeCalledWith(302, { Location: "/cookies" });
     });
 
     it("should update all cookies, with the cookie policy 'usage' as false when the user disables tracking", () => {
         const mockCookiePolicy: CookiePolicy = { essential: true, usage: false };
         const { req, res } = getMockRequestAndResponse({
             body: {
-                tracking: 'off',
+                tracking: "off",
             },
             mockWriteHeadFn: writeHeadMock,
         });
@@ -31,7 +31,7 @@ describe('cookies', () => {
         expect(setCookieSpy).toHaveBeenNthCalledWith(
             1,
             COOKIE_PREFERENCES_COOKIE,
-            'true',
+            "true",
             req,
             res,
             oneYearInSeconds,
@@ -46,14 +46,14 @@ describe('cookies', () => {
             oneYearInSeconds,
             false,
         );
-        expect(writeHeadMock).toBeCalledWith(302, { Location: '/cookies?settingsSaved=true' });
+        expect(writeHeadMock).toBeCalledWith(302, { Location: "/cookies?settingsSaved=true" });
     });
 
     it("should update all cookies, with the cookie policy 'usage' as true when the user enables tracking", () => {
         const mockCookiePolicy: CookiePolicy = { essential: true, usage: true };
         const { req, res } = getMockRequestAndResponse({
             body: {
-                tracking: 'on',
+                tracking: "on",
             },
             mockWriteHeadFn: writeHeadMock,
         });
@@ -61,7 +61,7 @@ describe('cookies', () => {
         expect(setCookieSpy).toHaveBeenNthCalledWith(
             1,
             COOKIE_PREFERENCES_COOKIE,
-            'true',
+            "true",
             req,
             res,
             oneYearInSeconds,
@@ -76,6 +76,6 @@ describe('cookies', () => {
             oneYearInSeconds,
             false,
         );
-        expect(writeHeadMock).toBeCalledWith(302, { Location: '/cookies?settingsSaved=true' });
+        expect(writeHeadMock).toBeCalledWith(302, { Location: "/cookies?settingsSaved=true" });
     });
 });
