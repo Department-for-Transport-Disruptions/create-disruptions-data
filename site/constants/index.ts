3d8de189896412538fd135efc95f26ef4f86a29f
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-03-08 09:44:33 +0000

    feat: Cookie banner (#17)

--- site/constants/index.ts
+++ site/constants/index.ts
@@ -30,3 +30,11 @@ export const DISRUPTION_REASONS: {
         reason: "Special Event",
     },
 ];
+
+export const COOKIES_POLICY_COOKIE = "cdd-cookies-policy";
+
+export const COOKIE_PREFERENCES_COOKIE = "cdd-cookie-preferences-set";
+
+export const oneYearInSeconds = 31556952;
+
+export const ID_TOKEN_COOKIE = "cdd-id-token";
