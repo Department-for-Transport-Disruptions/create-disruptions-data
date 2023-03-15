ba756ab944208dda08e5c02519368b9886246e3a
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-03-15 11:03:51 +0000

    remove use effect once

--- site/utils/index.ts
+++ site/utils/index.ts
@@ -22,40 +22,6 @@ export const redirectTo = (res: NextApiResponse | ServerResponse, location: stri
 export const getCsrfToken = (ctx: NextPageContext | NextPageContext): string =>
     (ctx.res as ResponseWithLocals)?.locals?.csrfToken ?? "";
 
-export const useEffectOnce = (effect: () => void | (() => void)) => {
-    const destroyFunc = useRef<void | (() => void)>();
-    const effectCalled = useRef(false);
-    const renderAfterCalled = useRef(false);
-    const [, setVal] = useState<number>(0);
-
-    if (effectCalled.current) {
-        renderAfterCalled.current = true;
-    }
-
-    useEffect(() => {
-        // only execute the effect first time around
-        if (!effectCalled.current) {
-            destroyFunc.current = effect();
-            effectCalled.current = true;
-        }
-
-        // this forces one render after the effect is run
-        setVal((val) => val + 1);
-
-        return () => {
-            // if the comp didn't render since the useEffect was called,
-            // we know it's the dummy React cycle
-            if (!renderAfterCalled.current) {
-                return;
-            }
-            if (destroyFunc.current) {
-                destroyFunc.current();
-            }
-        };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-};
-
 export const convertDateTimeToFormat = (dateOrTime: string, format: string) => dayjs(dateOrTime).format(format);
 
 export const formatTime = (time: string) => (time ? time.slice(0, -2) + ":" + time.slice(-2) : "");
