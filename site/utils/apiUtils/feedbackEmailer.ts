44f9cc9c922b617f2f2f5ea3bccb3e7773460518
alvak1 <karthik.alva@kpmg.co.uk>
2023-03-28 21:22:09 +0100

    Changes for feedback emailer

--- site/utils/apiUtils/feedbackEmailer.ts
+++ site/utils/apiUtils/feedbackEmailer.ts
@@ -1,6 +1,5 @@
-import Mail from "nodemailer/lib/mailer";
-
-import { SERVICE_EMAIL_ADDRESS, STAGE } from "../../constants";
+import { SendEmailCommand, SESClient } from "@aws-sdk/client-ses";
+import { STAGE } from "../../constants";
 import { Feedback } from "../../interfaces";
 
 export const buildFeedbackContent = (feedbackQuestions: Feedback[]): string => {
@@ -11,13 +10,33 @@ export const buildFeedbackContent = (feedbackQuestions: Feedback[]): string => {
     return questionsAndAnswers.join("\n");
 };
 
-export const setFeedbackMailOptions = (feedbackSubmitterEmailAddress: string, feedback: Feedback[]): Mail.Options => {
+export const setFeedbackMailOptions = (feedback: Feedback[]): SendEmailCommand => {
     const subject = STAGE === "prod" ? "Feedback received" : `${STAGE} - Feedback received`;
-    return {
-        from: SERVICE_EMAIL_ADDRESS,
-        to: SERVICE_EMAIL_ADDRESS,
-        cc: feedbackSubmitterEmailAddress,
-        subject: subject,
-        text: buildFeedbackContent(feedback),
-    };
+
+    return new SendEmailCommand({
+        Destination: {
+            /* required */
+            ToAddresses: process.env.EMAIL_ADDRESS ? [process.env.EMAIL_ADDRESS] : [],
+        },
+        Message: {
+            /* required */
+            Body: {
+                /* required */
+                Text: {
+                    Charset: "UTF-8",
+                    Data: buildFeedbackContent(feedback),
+                },
+            },
+            Subject: {
+                Charset: "UTF-8",
+                Data: subject,
+            },
+        },
+        Source: process.env.EMAIL_ADDRESS,
+        SourceArn: process.env.AWS_SES_TEST_IDENTITY,
+    });
+};
+
+export const createMailTransporter = (): SESClient => {
+    return new SESClient({ region: "us-east-1" });
 };
