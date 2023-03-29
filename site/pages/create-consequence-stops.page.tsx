e2a3b762677650bf479c58e323a13116c0f126d1
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-03-29 13:16:10 +0100

    adjust for data

--- site/pages/create-consequence-stops.page.tsx
+++ site/pages/create-consequence-stops.page.tsx
@@ -203,7 +203,7 @@ const CreateConsequenceStops = ({
                             getOptionValue={getOptionValue}
                             display="Stops Impacted"
                             displaySize="l"
-                            inputId="stops"
+                            inputId="stop"
                         />
 
                         <TextInput<StopsConsequence>
