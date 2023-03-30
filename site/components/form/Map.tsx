b7eb30ad161b096460516fd3cd0d4b9312027856
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-03-30 14:53:33 +0100

    rename

--- site/components/form/Map.tsx
+++ site/components/form/Map.tsx
@@ -1,13 +1,13 @@
 import { CSSProperties, ReactElement } from "react";
 import Map, { ViewState } from "react-map-gl";
 
-interface MapBoxProps {
+interface MapProps {
     initialViewState: Partial<ViewState>;
     style: CSSProperties;
     mapStyle: string;
 }
 
-const MapBox = ({ initialViewState, style, mapStyle }: MapBoxProps): ReactElement | null => {
+const MapBox = ({ initialViewState, style, mapStyle }: MapProps): ReactElement | null => {
     const mapboxAccessToken = process.env.MAP_BOX_ACCESS_TOKEN;
 
     return mapboxAccessToken ? (
