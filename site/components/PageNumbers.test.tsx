ab0ee8ea53f980fc157f6fdee9c62fcfa21459e8
RobCattonKPMG <robert.catton@kpmg.co.uk>
2023-03-30 16:39:59 +0100

    Changing functionality so nothing is rendered if there are no pages of disruptions

--- site/components/PageNumbers.test.tsx
+++ site/components/PageNumbers.test.tsx
@@ -3,6 +3,14 @@ import { describe, it, expect, vi } from "vitest";
 import PageNumbers from "./PageNumbers";
 
 describe("PageNumbers", () => {
+
+    it("should not render anything when there are no pages", () => {
+        const tree = renderer
+            .create(<PageNumbers currentPage={0} numberOfPages={0} setCurrentPage={vi.fn()} />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
     it("should render correctly when there are less than 10 pages", () => {
         const tree = renderer
             .create(<PageNumbers currentPage={1} numberOfPages={8} setCurrentPage={vi.fn()} />)
--- site/components/PageNumbers.tsx
+++ site/components/PageNumbers.tsx
@@ -228,63 +228,65 @@ const createPageNumbers = (
     return pageNumbers;
 };
 
-const PageNumbers = ({ currentPage, numberOfPages, setCurrentPage }: PageNumbersProps): ReactElement => (
-    <nav className="govuk-pagination" role="navigation">
-        {currentPage !== 1 ? (
-            <div className="govuk-pagination__prev">
-                <a
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setCurrentPage(currentPage - 1);
-                    }}
-                    className="govuk-link govuk-pagination__link"
-                    href=""
-                    rel="prev"
-                >
-                    <svg
-                        className="govuk-pagination__icon govuk-pagination__icon--prev"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="13"
-                        width="15"
-                        aria-hidden="true"
-                        focusable="false"
-                        viewBox="0 0 15 13"
+const PageNumbers = ({ currentPage, numberOfPages, setCurrentPage }: PageNumbersProps): ReactElement =>
+    numberOfPages === 0 ? (
+        <></>
+    ) : (
+        <nav className="govuk-pagination" role="navigation">
+            {currentPage !== 1 ? (
+                <div className="govuk-pagination__prev">
+                    <a
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setCurrentPage(currentPage - 1);
+                        }}
+                        className="govuk-link govuk-pagination__link"
+                        href=""
+                        rel="prev"
                     >
-                        <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
-                    </svg>
-                    <span className="govuk-pagination__link-title">Previous</span>
-                </a>
-            </div>
-        ) : null}
+                        <svg
+                            className="govuk-pagination__icon govuk-pagination__icon--prev"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="13"
+                            width="15"
+                            aria-hidden="true"
+                            focusable="false"
+                            viewBox="0 0 15 13"
+                        >
+                            <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
+                        </svg>
+                        <span className="govuk-pagination__link-title">Previous</span>
+                    </a>
+                </div>
+            ) : null}
 
-        <ul className="govuk-pagination__list"> {createPageNumbers(numberOfPages, currentPage, setCurrentPage)}</ul>
-        {currentPage !== numberOfPages ? (
-            <div className="govuk-pagination__next">
-                <a
-                    onClick={(e) => {
-                        e.preventDefault();
-                        setCurrentPage(currentPage + 1);
-                    }}
-                    className="govuk-link govuk-pagination__link"
-                    href=""
-                    rel="next"
-                >
-                    <span className="govuk-pagination__link-title">Next</span>{" "}
-                    <svg
-                        className="govuk-pagination__icon govuk-pagination__icon--next"
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="13"
-                        width="15"
-                        aria-hidden="true"
-                        focusable="false"
-                        viewBox="0 0 15 13"
+            <ul className="govuk-pagination__list"> {createPageNumbers(numberOfPages, currentPage, setCurrentPage)}</ul>
+            {currentPage !== numberOfPages ? (
+                <div className="govuk-pagination__next">
+                    <a
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setCurrentPage(currentPage + 1);
+                        }}
+                        className="govuk-link govuk-pagination__link"
+                        href=""
+                        rel="next"
                     >
-                        <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
-                    </svg>
-                </a>
-            </div>
-        ) : null}
-    </nav>
-);
-
+                        <span className="govuk-pagination__link-title">Next</span>{" "}
+                        <svg
+                            className="govuk-pagination__icon govuk-pagination__icon--next"
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="13"
+                            width="15"
+                            aria-hidden="true"
+                            focusable="false"
+                            viewBox="0 0 15 13"
+                        >
+                            <path d="m8.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
+                        </svg>
+                    </a>
+                </div>
+            ) : null}
+        </nav>
+    );
 export default PageNumbers;
