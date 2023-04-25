66e95960bd597d9c75f178b80665edfae583fe6c
RobCattonKPMG <robert.catton@kpmg.co.uk>
2023-04-25 10:14:37 +0100

    Removed eslint ignores

--- site/pages/api/create-consequence-operator.api.ts
+++ site/pages/api/create-consequence-operator.api.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { NextApiRequest, NextApiResponse } from "next";
 import {
     COOKIES_CONSEQUENCE_OPERATOR_ERRORS,
