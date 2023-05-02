f484cb1283f650bbc23ca76f5b1a69a084872a4e
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-05-02 15:47:10 +0100

    unit tests

---
+++ site/components/ReviewConsequenceTable.test.tsx
@@ -0,0 +1,61 @@
+import { EnvironmentReason, Severity, VehicleMode } from "@create-disruptions-data/shared-ts/enums";
+import renderer from "react-test-renderer";
+import { describe, it, expect, vi } from "vitest";
+import ReviewConsequenceTable from "./ReviewConsequenceTable";
+import { Consequence } from "../schemas/consequence.schema";
+import { Disruption } from "../schemas/disruption.schema";
+
+const previousConsequencesInformation: Consequence = {
+    vehicleMode: VehicleMode.bus,
+    consequenceType: "networkWide",
+    consequenceIndex: 0,
+    disruptionId: "1",
+    description: "The road is closed for the following reasons: Example, example, example, example",
+    removeFromJourneyPlanners: "yes",
+    disruptionDelay: "33",
+    disruptionSeverity: Severity.severe,
+};
+
+const previousDisruptionInformation: Disruption = {
+    disruptionType: "planned",
+    disruptionId: "2",
+    summary: "Road closure due to flooding and cattle on road and no sign of movement example example example etc etc",
+    description:
+        "Road closure due to flooding and cattle on road and no sign of movement example example example etc etc",
+    associatedLink: "https://www.flooding.com",
+    disruptionReason: EnvironmentReason.flooding,
+    validity: [
+        {
+            disruptionStartDate: "13/01/2022",
+            disruptionStartTime: "1200",
+            disruptionEndDate: "14/01/2022",
+            disruptionEndTime: "1400",
+            disruptionNoEndDateTime: "",
+        },
+    ],
+    publishStartDate: "13/01/2022",
+    publishStartTime: "1300",
+    publishEndDate: "13/01/2022",
+    publishEndTime: "1400",
+    disruptionStartDate: "15/01/2022",
+    disruptionStartTime: "1200",
+    disruptionEndDate: "17/01/2022",
+    disruptionEndTime: "1400",
+    disruptionNoEndDateTime: "",
+    consequences: [previousConsequencesInformation],
+};
+
+describe("ReviewConsequenceTable", () => {
+    it("should render the table with data", () => {
+        const tree = renderer
+            .create(
+                <ReviewConsequenceTable
+                    consequence={previousConsequencesInformation}
+                    disruption={previousDisruptionInformation}
+                    deleteActionHandler={vi.fn()}
+                />,
+            )
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
