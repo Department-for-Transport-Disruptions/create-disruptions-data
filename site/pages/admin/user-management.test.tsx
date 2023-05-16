e3a1d0d547965988268b30aeeb35c4d20a61fb06
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-05-16 15:02:13 +0100

    feat: Admin remove (#104)

--- site/pages/admin/user-management.test.tsx
+++ site/pages/admin/user-management.test.tsx
@@ -16,6 +16,7 @@ const withInputs: UserManagementPageProps = {
             userStatus: "CONFIRMED",
             group: UserGroups.orgAdmins,
             organisation: "admin",
+            username: "2f99b92e-a86f-4457-a2dc-923db4781c52",
         },
     ],
 };
---
+++ site/pages/api/admin/delete-user.test.ts
@@ -0,0 +1,110 @@
+import { describe, it, expect, afterEach, vi } from "vitest";
+import deleteUser from "./delete-user.api";
+import { ERROR_PATH, USER_MANAGEMENT_PAGE_PATH } from "../../../constants";
+import * as cognito from "../../../data/cognito";
+import { DEFAULT_ORG_ID, getMockRequestAndResponse } from "../../../testData/mockData";
+
+describe("login", () => {
+    const writeHeadMock = vi.fn();
+
+    const deleteAdminUserSpy = vi.spyOn(cognito, "deleteUser");
+
+    const getUserDetailsSpy = vi.spyOn(cognito, "getUserDetails");
+
+    vi.mock("../../../data/cognito", () => ({
+        deleteUser: vi.fn(),
+        getUserDetails: vi.fn(),
+    }));
+
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+
+    it("should redirect to /admin/user-management if delete was a success", async () => {
+        deleteAdminUserSpy.mockImplementation(() =>
+            Promise.resolve({
+                body: {},
+                $metadata: { httpStatusCode: 302 },
+            }),
+        );
+
+        getUserDetailsSpy.mockImplementation(() =>
+            Promise.resolve({
+                body: {},
+                $metadata: { httpStatusCode: 302 },
+                Username: "2f99b92e-a86f-4457-a2dc-923db4781c52",
+                UserAttributes: [
+                    {
+                        Name: "custom:orgId",
+                        Value: DEFAULT_ORG_ID,
+                    },
+                ],
+            }),
+        );
+
+        const { req, res } = getMockRequestAndResponse({
+            body: {
+                username: "2f99b92e-a86f-4457-a2dc-923db4781c52",
+            },
+            mockWriteHeadFn: writeHeadMock,
+        });
+
+        await deleteUser(req, res);
+
+        expect(writeHeadMock).toBeCalledWith(302, { Location: USER_MANAGEMENT_PAGE_PATH });
+    });
+
+    it("should redirect to /500 if organisation ids do not match", async () => {
+        getUserDetailsSpy.mockImplementation(() =>
+            Promise.resolve({
+                body: {},
+                $metadata: { httpStatusCode: 302 },
+                Username: "2f99b92e-a86f-4457-a2dc-923db4781c52",
+                UserAttributes: [
+                    {
+                        Name: "custom:orgId",
+                        Value: "1234",
+                    },
+                ],
+            }),
+        );
+
+        const { req, res } = getMockRequestAndResponse({
+            body: {
+                username: "2f99b92e-a86f-4457-a2dc-923db4781c52",
+            },
+            mockWriteHeadFn: writeHeadMock,
+        });
+
+        await deleteUser(req, res);
+
+        expect(deleteAdminUserSpy).not.toBeCalled();
+        expect(writeHeadMock).toBeCalledWith(302, { Location: ERROR_PATH });
+    });
+
+    it("should redirect to /500 if delete operation failed", async () => {
+        deleteAdminUserSpy.mockImplementation(() => {
+            throw new Error("invalid", {
+                cause: "Invalid",
+            });
+        });
+
+        getUserDetailsSpy.mockImplementation(() =>
+            Promise.resolve({
+                body: {},
+                $metadata: { httpStatusCode: 400 },
+                Username: "",
+                UserAttributes: [],
+            }),
+        );
+
+        const { req, res } = getMockRequestAndResponse({
+            body: {},
+            mockWriteHeadFn: writeHeadMock,
+        });
+
+        await deleteUser(req, res);
+
+        expect(writeHeadMock).toBeCalledWith(302, { Location: ERROR_PATH });
+    });
+});
