07abc19658f3738e85ad3de1a6481d682d7ec745
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-05-24 10:04:04 +0100

    API Changes

--- site/pages/api/create-social-media-post.api.ts
+++ site/pages/api/create-social-media-post.api.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { readFile } from "fs/promises";
 import {
     COOKIES_SOCIAL_MEDIA_ERRORS,
     CREATE_SOCIAL_MEDIA_POST_PAGE_PATH,
@@ -16,75 +17,62 @@ import {
 } from "../../utils/apiUtils";
 import { getSession } from "../../utils/apiUtils/auth";
 import { formParse } from "../../utils/apiUtils/fileUpload";
-import { readFile } from "fs/promises";
 
 const createSocialMediaPost = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
     try {
-        const { files } = await formParse(req);
-        const image = Array.isArray(files["image"]) ? files["image"][0] : files["image"];
-
-        const parsedBody = socialMediaPostSchema.parse({ ...req.body, image });
-
         const session = getSession(req);
-
+        console.log(JSON.stringify(req.body));
         if (!session) {
             throw new Error("No session found");
         }
 
-        if (parsedBody.image) {
-            const imageContents = await readFile(parsedBody.image?.filepath);
+        const body = req.body as SocialMediaPost;
 
-            await putItem(process.env.IMAGE_BUCKET_NAME || "", "test.png", imageContents);
+        if (!process.env.IMAGE_BUCKET_NAME) {
+            throw new Error("No image bucket to upload image to");
         }
 
-        res.send(JSON.stringify(req.body));
+        const { files } = await formParse(req);
+        const image = Array.isArray(files["image"]) ? files["image"][0] : files["image"];
 
+        const validatedBody = socialMediaPostSchema.safeParse({ ...body, image });
+
+        if (!validatedBody.success) {
+            setCookieOnResponseObject(
+                COOKIES_SOCIAL_MEDIA_ERRORS,
+                JSON.stringify({
+                    inputs: body,
+                    errors: flattenZodErrors(validatedBody.error),
+                }),
+                res,
+            );
+
+            redirectTo(res, `${CREATE_SOCIAL_MEDIA_POST_PAGE_PATH}/${body.disruptionId}/${body.socialMediaPostIndex}}`);
+            return;
+        }
+
+        if (validatedBody.data.image) {
+            const imageContents = await readFile(validatedBody.data.image?.filepath);
+
+            console.log(process.env.IMAGE_BUCKET_NAME);
+            await putItem(
+                process.env.IMAGE_BUCKET_NAME || "",
+                `${session.orgId}#${validatedBody.data.disruptionId}#${
+                    validatedBody.data.socialMediaPostIndex
+                }.${validatedBody.data.image.type.replace("image/", "")}`,
+                imageContents,
+            );
+        }
+
+        await upsertSocialMediaPost(validatedBody.data, session.orgId);
+
+        destroyCookieOnResponseObject(COOKIES_SOCIAL_MEDIA_ERRORS, res);
+        redirectTo(res, `${REVIEW_DISRUPTION_PAGE_PATH}/${body.disruptionId}`);
         return;
-        // const body = req.body as SocialMediaPost;
-
-        // if (!body.disruptionId) {
-        //     throw new Error("No disruptionId found");
-        // }
-
-        // if (!process.env.IMAGE_BUCKET_NAME) {
-        //     throw new Error("No image bucket to upload image to");
-        // }
-
-        // const session = getSession(req);
-
-        // if (!session) {
-        //     throw new Error("No session found");
-        // }
-
-        // const validatedBody = socialMediaPostSchema.safeParse(body);
-
-        // if (!validatedBody.success) {
-        //     setCookieOnResponseObject(
-        //         COOKIES_SOCIAL_MEDIA_ERRORS,
-        //         JSON.stringify({
-        //             inputs: body,
-        //             errors: flattenZodErrors(validatedBody.error),
-        //         }),
-        //         res,
-        //     );
-
-        //     redirectTo(res, `${CREATE_SOCIAL_MEDIA_POST_PAGE_PATH}/${body.disruptionId}/${body.socialMediaPostIndex}}`);
-        //     return;
-        // }
-        // await putItem(
-        //     process.env.IMAGE_BUCKET_NAME,
-        //     `${session.orgId}#${body.disruptionId}#${body.socialMediaPostIndex}.${body.image.type}`,
-        //     Buffer.from(body.image, "binary"),
-        // );
-        // await upsertSocialMediaPost(validatedBody.data, session.orgId);
-
-        // destroyCookieOnResponseObject(COOKIES_SOCIAL_MEDIA_ERRORS, res);
-        // redirectTo(res, `${REVIEW_DISRUPTION_PAGE_PATH}/${body.disruptionId}`);
-        // return;
     } catch (e) {
         if (e instanceof Error) {
             const message = "There was a problem creating a social media post.";
-            redirectToError(res, message, "api.create-social-media-posy", e);
+            redirectToError(res, message, "api.create-social-media-post", e);
             return;
         }
 
--- site/schemas/social-media.schema.ts
+++ site/schemas/social-media.schema.ts
@@ -1,18 +1,18 @@
+import { File } from "formidable";
 import { z } from "zod";
 import { ACCEPTED_IMAGE_TYPES, MAX_FILE_SIZE } from "../constants";
 import { setZodDefaultError, zodDate, zodTime } from "../utils";
-import { File } from "formidable";
 
 export const socialMediaPostSchema = z.object({
-    // disruptionId: z.string().uuid(),
-    // messageContent: z.string(setZodDefaultError("Enter a message content for this social media post")).min(1).max(200, {
-    //     message: "Message content must not exceed 200 characters",
-    // }),
-    // socialAccount: z.string(setZodDefaultError("Select a social account")),
-    // hootsuiteProfile: z.string(setZodDefaultError("Select a Hootsuite profile")),
-    // publishDate: zodDate("Enter a publish date for the social media post"),
-    // publishTime: zodTime("Enter a publish time for the social media post"),
-    // socialMediaPostIndex: z.number().default(0),
+    disruptionId: z.string().uuid(),
+    messageContent: z.string(setZodDefaultError("Enter a message content for this social media post")).min(1).max(200, {
+        message: "Message content must not exceed 200 characters",
+    }),
+    socialAccount: z.string(setZodDefaultError("Select a social account")),
+    hootsuiteProfile: z.string(setZodDefaultError("Select a Hootsuite profile")),
+    publishDate: zodDate("Enter a publish date for the social media post"),
+    publishTime: zodTime("Enter a publish time for the social media post"),
+    socialMediaPostIndex: z.number().default(0),
     image: z
         .any()
         .refine((file: File) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
@@ -22,15 +22,9 @@ export const socialMediaPostSchema = z.object({
         )
         .transform((file: File) => ({
             filepath: file.filepath,
+            type: file.mimetype || "",
         }))
         .optional(),
 });
 
 export type SocialMediaPost = z.infer<typeof socialMediaPostSchema>;
-
-// export const imageSchema = z.instanceof(File).refine((file) => file.size <= MAX_FILE_SIZE);
-// .refine((file: File) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
-// .refine(
-//     (file: File) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-//     "Only .jpg, .jpeg, .png and .webp formats are supported.",
-// );
