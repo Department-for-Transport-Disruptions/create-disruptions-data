b8b06e73795c04972f01beba712675739af5510f
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-06-19 13:49:41 +0100

    refactor

--- site/data/hoostuite.ts
+++ site/data/hoostuite.ts
@@ -32,9 +32,10 @@ export const getHootsuiteData = async (
         const idToken = cookies[COOKIES_ID_TOKEN];
         const refreshToken = cookies[COOKIES_REFRESH_TOKEN];
 
-        const [clientId, clientSecret, keys] = await Promise.all([
+        const [clientId, clientSecret, keys, tokensByOrganisation] = await Promise.all([
             getParameter(`/social/hootsuite/client_id`),
             getParameter(`/social/hootsuite/client_secret`),
+            getParametersByPath(`/social/${orgId}/hootsuite`),
             getParametersByPath(`/social/${orgId}/hootsuite`),
         ]);
 
@@ -53,7 +54,6 @@ export const getHootsuiteData = async (
                 putParameter(`/${username}/refresh-token`, refreshToken, "SecureString", true),
             ]);
 
-        const tokensByOrganisation = await getParametersByPath(`/social/${orgId}/hootsuite`);
         const refreshTokens = tokensByOrganisation?.Parameters?.map((token) => {
             return {
                 value: token.Value,
