086d400c6efe595321cffcef095940974fc6a594
alvak1 <karthik.alva@kpmg.co.uk>
2023-07-13 15:07:38 +0100

    Fix for issue with reverting datasource when org is updated by sys admin

--- site/pages/sysadmin/org.page.tsx
+++ site/pages/sysadmin/org.page.tsx
@@ -117,6 +117,7 @@ const ManageOrgs = (props: PageState<Partial<ManageOrgProps>>): ReactElement => 
 
                 <input type="hidden" name="PK" value={pageState.inputs.PK} />
                 <input type="hidden" name="adminAreaCodes" value={pageState.inputs.adminAreaCodes ?? []} />
+                <input type="hidden" name="mode" value={JSON.stringify(pageState.inputs.mode)} />
                 <button className="govuk-button mt-2" data-module="govuk-button">
                     {pageState.inputs.PK ? "Update" : "Add"}
                 </button>
@@ -160,12 +161,16 @@ export const getServerSideProps = async (
         fetchAdminAreaCodes(),
     ]);
 
-    const pageState = getPageState<Organisation>(
-        errorCookie,
-        organisationSchema,
-        "ID",
-        !errorCookie && orgId && orgInfo ? orgInfo : undefined,
-    );
+    let pageState: PageState<Partial<Organisation>> = {
+        errors: [],
+        inputs: {},
+    };
+
+    if (errorCookie) {
+        pageState = JSON.parse(errorCookie) as PageState<Partial<Organisation>>;
+    } else if (orgInfo) {
+        pageState.inputs = orgInfo;
+    }
 
     const parsedCodes = z.array(areaCodeSchema).safeParse(areaCodes);
     return {
