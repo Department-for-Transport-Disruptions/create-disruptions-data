6ca65b8386f829318cf74b045a5c56ee8d0b84af
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-08-25 10:20:58 +0100

    fix: Consequence button not greyed out (#229)

--- site/pages/review-disruption/[disruptionId].page.tsx
+++ site/pages/review-disruption/[disruptionId].page.tsx
@@ -542,13 +542,17 @@ const ReviewDisruption = ({ disruption, csrfToken, errors, canPublish }: ReviewD
                                 </div>
                             ))}
                         </div>
+
                         <Link
-                            role="button"
                             href={{
                                 pathname: `${TYPE_OF_CONSEQUENCE_PAGE_PATH}/${disruption.disruptionId}/${nextIndex}`,
                                 query: { return: REVIEW_DISRUPTION_PAGE_PATH },
                             }}
-                            className="govuk-button mt-2 govuk-button--secondary"
+                            className={`govuk-button mt-2 govuk-button--secondary ${
+                                disruption.consequences && disruption.consequences.length >= 10
+                                    ? "pointer-events-none govuk-button--disabled"
+                                    : ""
+                            }`}
                         >
                             Add another consequence
                         </Link>
