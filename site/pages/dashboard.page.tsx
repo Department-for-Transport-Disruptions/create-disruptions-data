d69b8f9dbf3793d4f3c63ee6e72386a6efac29d3
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2023-09-01 13:03:16 +0100

    feat: Templates Link

--- site/pages/dashboard.page.tsx
+++ site/pages/dashboard.page.tsx
@@ -254,6 +254,9 @@ const Dashboard = ({
             <Link className="govuk-link" href="/view-all-disruptions?draft=true">
                 <h2 className="govuk-heading-s text-govBlue">Draft disruptions</h2>
             </Link>
+            <Link className="govuk-link" href="/view-all-templates">
+                <h2 className="govuk-heading-s text-govBlue">Templates</h2>
+            </Link>
         </BaseLayout>
     );
 };
