90ac565d90e8f8de75be80d37024293211f019b0
alvak1 <95031756+alvak1@users.noreply.github.com>
2023-09-04 17:27:49 +0100

    Sorting functionality in view all disruption page (#240)
    * in progress
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page
    
    * Changes for sorting start and end columns in view all disruptions page

---
+++ site/components/form/SortableTable.tsx
@@ -0,0 +1,128 @@
+import { ReactElement, ReactNode, useEffect, useState } from "react";
+import PageNumbers from "../layout/PageNumbers";
+
+export interface TableColumn<T> {
+    displayName: string;
+    key: keyof T;
+    sortable?: boolean;
+}
+
+interface SortableTableProps<T> {
+    columns: TableColumn<T>[];
+    rows: T[];
+    sortFunction?: (rows: T[], sortField: keyof T, sortOrder: SortOrder) => T[];
+}
+
+export enum SortOrder {
+    asc = "asc",
+    desc = "desc",
+}
+
+const getPages = <T extends object>(pageNumber: number, rows: T[]): T[] => {
+    const startPoint = (pageNumber - 1) * 10;
+    const endPoint = pageNumber * 10;
+    return rows.slice(startPoint, endPoint);
+};
+
+const formatRows = <T extends object>(rows: T[], currentPage: number) => {
+    const pages = getPages(currentPage, rows);
+
+    return pages;
+};
+
+const SortableTable = <T extends object>({ columns, rows, sortFunction }: SortableTableProps<T>): ReactElement => {
+    const [sortedField, setSortedField] = useState<keyof T | null>(null);
+    const [sortOrder, setSortOrder] = useState(SortOrder.asc);
+    const [sortFlag, setSortFlag] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [displayRows, setDisplayRows] = useState<T[]>(rows);
+    const numberOfPages = Math.ceil(rows.length / 10);
+
+    const sortData = (field: keyof T) => {
+        if (sortedField === field) {
+            setSortOrder(sortOrder === SortOrder.asc ? SortOrder.desc : SortOrder.asc);
+            setSortFlag(!sortFlag);
+        } else {
+            setSortedField(field);
+            setSortOrder(SortOrder.asc);
+            setSortFlag(!sortFlag);
+        }
+    };
+
+    useEffect(() => {
+        setDisplayRows(rows);
+    }, [rows]);
+
+    const getArrowHeads = (field: keyof T) => {
+        if (!sortedField || sortedField !== field) {
+            return (
+                <div className="flex flex-col pl-1 text-arrow">
+                    <span className="-mb-1">&#x25B2;</span>
+                    <span>&#x25BC;</span>
+                </div>
+            );
+        } else {
+            return sortOrder === SortOrder.asc ? (
+                <span className="pl-1 govuk-!-font-size-14">&#x25B2;</span>
+            ) : (
+                <span className="pl-1 govuk-!-font-size-14">&#x25BC;</span>
+            );
+        }
+    };
+
+    useEffect(() => {
+        if (sortedField && sortFunction) {
+            const sortedData = sortFunction(displayRows, sortedField, sortOrder);
+            setDisplayRows([...sortedData]);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [sortFlag]);
+
+    return (
+        <>
+            <table className="govuk-table">
+                <thead className="govuk-table__head">
+                    <tr className="govuk-table__row">
+                        {columns.map((column) => {
+                            return column.sortable ? (
+                                <th
+                                    scope="col"
+                                    className="govuk-table__header align-middle px-2"
+                                    key={`header-${column.displayName}`}
+                                    onClick={() => sortData(column.key)}
+                                >
+                                    <div className="flex flex-row items-center">
+                                        {column.displayName}
+                                        {getArrowHeads(column.key)}
+                                    </div>
+                                </th>
+                            ) : (
+                                <th
+                                    scope="col"
+                                    className="govuk-table__header align-middle px-2"
+                                    key={`header-${column.displayName}`}
+                                >
+                                    {column.displayName}
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+                <tbody className="govuk-table__body">
+                    {formatRows(displayRows, currentPage).map((row, index) => (
+                        <tr className="govuk-table__row" key={`row-${index}`}>
+                            {columns.map((column, i) => (
+                                <td className={"govuk-table__cell align-middle px-2"} key={`row-${index}${i}`}>
+                                    {row[column.key] as ReactNode}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <PageNumbers numberOfPages={numberOfPages} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        </>
+    );
+};
+
+export default SortableTable;
--- site/pages/view-all-disruptions.page.tsx
+++ site/pages/view-all-disruptions.page.tsx
@@ -16,9 +16,9 @@ import { z } from "zod";
 import { randomUUID } from "crypto";
 import DateSelector from "../components/form/DateSelector";
 import Select from "../components/form/Select";
+import SortableTable, { SortOrder, TableColumn } from "../components/form/SortableTable";
 import Table from "../components/form/Table";
 import { BaseLayout } from "../components/layout/Layout";
-import PageNumbers from "../components/layout/PageNumbers";
 import PDFDoc from "../components/pdf/DownloadPDF";
 import ExportPopUp from "../components/popup/ExportPopup";
 import OperatorSearch from "../components/search/OperatorSearch";
@@ -34,13 +34,13 @@ import {
 } from "../constants";
 import { fetchOperators, fetchServices } from "../data/refDataApi";
 import { Operator, ServiceApiResponse } from "../schemas/consequence.schema";
-import { disruptionsTableSchema, ExportDisruptionData, exportDisruptionsSchema } from "../schemas/disruption.schema";
+import { ExportDisruptionData, disruptionsTableSchema, exportDisruptionsSchema } from "../schemas/disruption.schema";
 import {
+    SortedDisruption,
     getDisplayByValue,
-    splitCamelCaseToString,
     getServiceLabel,
     getSortedDisruptionFinalEndDate,
-    SortedDisruption,
+    splitCamelCaseToString,
 } from "../utils";
 import { getSessionWithOrgDetail } from "../utils/apiUtils/auth";
 import {
@@ -100,6 +100,31 @@ export interface Filter {
     searchText?: string;
 }
 
+export interface DisruptionTable {
+    id: JSX.Element;
+    summary: string;
+    modes: string;
+    start: string;
+    end: string;
+    severity: string;
+    status: string;
+}
+
+const sortFunction = (disruptions: DisruptionTable[], sortField: keyof DisruptionTable, sortOrder: SortOrder) => {
+    return disruptions.sort((a, b) => {
+        const aValue = getFormattedDate(a[sortField] === "No end time" ? "01/01/1900" : (a[sortField] as string));
+        const bValue = getFormattedDate(b[sortField] === "No end time" ? "01/01/1900" : (b[sortField] as string));
+
+        if (aValue.isBefore(bValue)) {
+            return sortOrder === SortOrder.asc ? -1 : 1;
+        } else if (aValue.isAfter(bValue)) {
+            return sortOrder === SortOrder.asc ? 1 : -1;
+        } else {
+            return 0;
+        }
+    });
+};
+
 export const getDisruptionStatus = (disruption: SortedDisruption): Progress => {
     if (disruption.publishStatus === PublishStatus.draft) {
         return Progress.draft;
@@ -144,9 +169,8 @@ export const disruptionIsClosingOrClosed = (disruptionEndDate: Dayjs, today: Day
     return Progress.open;
 };
 
-const formatDisruptionsIntoRows = (disruptions: TableDisruption[], currentPage: number) => {
-    const pageOfDisruptions = getPageOfDisruptions(currentPage, disruptions);
-    return pageOfDisruptions.map((disruption) => {
+const formatDisruptionsIntoRows = (disruptions: TableDisruption[]): DisruptionTable[] => {
+    return disruptions.map((disruption) => {
         const earliestPeriod: {
             startTime: string;
             endTime: string | null;
@@ -154,7 +178,7 @@ const formatDisruptionsIntoRows = (disruptions: TableDisruption[], currentPage: 
         const latestPeriod: string | null = disruption.validityPeriods[disruption.validityPeriods.length - 1].endTime;
 
         return {
-            header: (
+            id: (
                 <Link
                     className="govuk-link"
                     href={
@@ -178,22 +202,14 @@ const formatDisruptionsIntoRows = (disruptions: TableDisruption[], currentPage: 
                     {disruption.displayId}
                 </Link>
             ),
-            cells: [
-                disruption.summary,
-                disruption.modes.map((mode) => splitCamelCaseToString(mode)).join(", ") || "N/A",
-                convertDateTimeToFormat(earliestPeriod.startTime),
-                !!latestPeriod ? convertDateTimeToFormat(latestPeriod) : "No end time",
-                splitCamelCaseToString(disruption.severity),
-                splitCamelCaseToString(disruption.status),
-            ],
+            summary: disruption.summary,
+            modes: disruption.modes.map((mode) => splitCamelCaseToString(mode)).join(", ") || "N/A",
+            start: convertDateTimeToFormat(earliestPeriod.startTime),
+            end: !!latestPeriod ? convertDateTimeToFormat(latestPeriod) : "No end time",
+            severity: splitCamelCaseToString(disruption.severity),
+            status: splitCamelCaseToString(disruption.status),
         };
     });
-};
-
-const getPageOfDisruptions = (pageNumber: number, disruptions: TableDisruption[]): TableDisruption[] => {
-    const startPoint = (pageNumber - 1) * 10;
-    const endPoint = pageNumber * 10;
-    return disruptions.slice(startPoint, endPoint);
 };
 
 export const getWorstSeverity = (severitys: Severity[]): Severity => {
@@ -339,12 +355,10 @@ const applyFiltersToDisruptions = (
     disruptions: TableDisruption[],
     setDisruptionsToDisplay: Dispatch<SetStateAction<TableDisruption[]>>,
     filter: Filter,
-    setNumberOfDisruptionsPages: Dispatch<SetStateAction<number>>,
 ): void => {
     const disruptionsToDisplay = filterDisruptions(disruptions, filter);
 
     setDisruptionsToDisplay(disruptionsToDisplay);
-    setNumberOfDisruptionsPages(Math.ceil(disruptionsToDisplay.length / 10));
 };
 
 const filterIsEmpty = (filter: Filter): boolean =>
@@ -373,8 +387,6 @@ const ViewAllDisruptions = ({
     filterStatus,
     enableLoadingSpinnerOnPageLoad = true,
 }: ViewAllDisruptionsProps): ReactElement => {
-    const [numberOfDisruptionsPages, setNumberOfDisruptionsPages] = useState<number>(0);
-    const [currentPage, setCurrentPage] = useState(1);
     const [selectedServices, setSelectedServices] = useState<Service[]>([]);
     const [selectedOperators, setSelectedOperators] = useState<ConsequenceOperators[]>([]);
 
@@ -472,13 +484,11 @@ const ViewAllDisruptions = ({
             const disruptions = await getDisruptionData();
             setDisruptionsToDisplay(disruptions);
             setDisruptions(disruptions);
-            setNumberOfDisruptionsPages(Math.ceil(disruptions.length / 10));
             setLoadPage(false);
         };
 
         fetchData().catch(() => {
             setDisruptionsToDisplay([]);
-            setNumberOfDisruptionsPages(0);
             setLoadPage(false);
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -520,22 +530,12 @@ const ViewAllDisruptions = ({
 
     useEffect(() => {
         setFilter({ ...filter, services: selectedServices });
-        applyFiltersToDisruptions(
-            disruptions,
-            setDisruptionsToDisplay,
-            { ...filter, services: selectedServices },
-            setNumberOfDisruptionsPages,
-        ); // eslint-disable-next-line react-hooks/exhaustive-deps
+        applyFiltersToDisruptions(disruptions, setDisruptionsToDisplay, { ...filter, services: selectedServices }); // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedServices]);
 
     useEffect(() => {
         setFilter({ ...filter, searchText });
-        applyFiltersToDisruptions(
-            disruptions,
-            setDisruptionsToDisplay,
-            { ...filter, searchText },
-            setNumberOfDisruptionsPages,
-        ); // eslint-disable-next-line react-hooks/exhaustive-deps
+        applyFiltersToDisruptions(disruptions, setDisruptionsToDisplay, { ...filter, searchText }); // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [searchText]);
 
     useEffect(() => {
@@ -555,21 +555,17 @@ const ViewAllDisruptions = ({
             operators: disruptionOperatorsToSet,
         });
 
-        applyFiltersToDisruptions(
-            disruptions,
-            setDisruptionsToDisplay,
-            { ...filter, operators: disruptionOperatorsToSet },
-            setNumberOfDisruptionsPages,
-        ); // eslint-disable-next-line react-hooks/exhaustive-deps
+        applyFiltersToDisruptions(disruptions, setDisruptionsToDisplay, {
+            ...filter,
+            operators: disruptionOperatorsToSet,
+        }); // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [selectedOperators]);
 
     useEffect(() => {
         if (filterIsEmpty(filter)) {
             setDisruptionsToDisplay(disruptions);
-            setNumberOfDisruptionsPages(Math.ceil(disruptions.length / 10));
         } else {
-            applyFiltersToDisruptions(disruptions, setDisruptionsToDisplay, filter, setNumberOfDisruptionsPages);
-            setCurrentPage(1);
+            applyFiltersToDisruptions(disruptions, setDisruptionsToDisplay, filter);
         } // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [filter]);
 
@@ -680,6 +676,39 @@ const ViewAllDisruptions = ({
         });
     };
 
+    const columns: TableColumn<DisruptionTable>[] = [
+        {
+            displayName: "ID",
+            key: "id",
+        },
+        {
+            displayName: "Summary",
+            key: "summary",
+        },
+        {
+            displayName: "Modes",
+            key: "modes",
+        },
+        {
+            displayName: "Start",
+            key: "start",
+            sortable: true,
+        },
+        {
+            displayName: "End",
+            key: "end",
+            sortable: true,
+        },
+        {
+            displayName: "Severity",
+            key: "severity",
+        },
+        {
+            displayName: "Status",
+            key: "status",
+        },
+    ];
+
     return (
         <BaseLayout title={title} description={description}>
             {popUpState ? <ExportPopUp confirmHandler={exportHandler} closePopUp={cancelActionHandler} /> : null}
@@ -714,7 +743,6 @@ const ViewAllDisruptions = ({
                             if (showFilters) {
                                 setFilter({ services: [], operators: [] });
                                 setDisruptionsToDisplay(disruptions);
-                                setNumberOfDisruptionsPages(Math.ceil(disruptions.length / 10));
                                 setClearButtonClicked(true);
                                 setShowFilters(false);
                             } else {
@@ -738,8 +766,6 @@ const ViewAllDisruptions = ({
                                 setFilter({ services: [], operators: [] });
                                 setDisruptionsToDisplay(disruptions);
                                 setClearButtonClicked(true);
-                                setNumberOfDisruptionsPages(Math.ceil(disruptions.length / 10));
-                                setCurrentPage(1);
                             }}
                         >
                             Clear filters
@@ -943,30 +969,18 @@ const ViewAllDisruptions = ({
 
             {enableLoadingSpinnerOnPageLoad ? (
                 <LoadingBox loading={loadPage}>
-                    <>
-                        <Table
-                            columns={["ID", "Summary", "Modes", "Starts", "Ends", "Severity", "Status"]}
-                            rows={formatDisruptionsIntoRows(disruptionsToDisplay, currentPage)}
-                        />
-                        <PageNumbers
-                            numberOfPages={numberOfDisruptionsPages}
-                            currentPage={currentPage}
-                            setCurrentPage={setCurrentPage}
-                        />
-                    </>
+                    <SortableTable
+                        columns={columns}
+                        rows={formatDisruptionsIntoRows(disruptionsToDisplay)}
+                        sortFunction={sortFunction}
+                    />
                 </LoadingBox>
             ) : (
-                <>
-                    <Table
-                        columns={["ID", "Summary", "Modes", "Starts", "Ends", "Severity", "Status"]}
-                        rows={formatDisruptionsIntoRows(disruptionsToDisplay, currentPage)}
-                    />
-                    <PageNumbers
-                        numberOfPages={numberOfDisruptionsPages}
-                        currentPage={currentPage}
-                        setCurrentPage={setCurrentPage}
-                    />
-                </>
+                <SortableTable
+                    columns={columns}
+                    rows={formatDisruptionsIntoRows(disruptionsToDisplay)}
+                    sortFunction={sortFunction}
+                />
             )}
         </BaseLayout>
     );
