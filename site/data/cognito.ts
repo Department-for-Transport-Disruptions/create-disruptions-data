503cc00739fb1ac894edc77cce8b96ff5ac475ca
Laurence Jones <laurence.jones@doza.consulting>
2023-09-19 17:02:28 +0100

    increase limit for cognito query

--- site/data/cognito.ts
+++ site/data/cognito.ts
@@ -200,7 +200,7 @@ export const listUsersWithGroups = async () => {
         await Promise.all(
             groupList.Groups?.map(async (group) => {
                 const usersRequest = await cognito.send(
-                    new ListUsersInGroupCommand({ UserPoolId: userPoolId, GroupName: group.GroupName }),
+                    new ListUsersInGroupCommand({ UserPoolId: userPoolId, GroupName: group.GroupName, Limit: 60 }),
                 );
 
                 usersRequest.Users?.forEach((user) => {
