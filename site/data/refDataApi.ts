403d9637c805b7d2c57dbd08ad2e505356b784fd
Deanna Sharma <deanna.sharma@doza.consulting>
2023-10-20 11:10:36 +0100

    fix

--- site/data/refDataApi.ts
+++ site/data/refDataApi.ts
@@ -281,7 +281,7 @@ export const fetchOperatorUserNocCodes = async (input: FetchOperatorUserNocCodes
         }
 
         if (operatorData.length > 0) {
-            return operatorData.concat(parseResult.data);
+            operatorData.concat(parseResult.data);
         } else operatorData = parseResult.data;
     }
 
