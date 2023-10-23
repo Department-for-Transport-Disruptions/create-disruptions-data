15293c1162f29b02cf7f8157ac1bacd73f86ec70
Deanna Sharma <deanna.sharma@doza.consulting>
2023-10-23 11:27:39 +0100

    updated add user page to use template

--- site/components/user-page-template.tsx
+++ site/components/user-page-template.tsx
@@ -1,7 +1,7 @@
 import { UserGroups } from "@create-disruptions-data/shared-ts/enums";
 import Link from "next/link";
-import { SyntheticEvent } from "react";
-import { createFilter } from "react-select";
+import { Dispatch, SetStateAction, SyntheticEvent } from "react";
+import { createFilter, SingleValue } from "react-select";
 import type { FilterOptionOption } from "react-select/dist/declarations/src/filters";
 import CsrfForm from "./form/CsrfForm";
 import ErrorSummary from "./form/ErrorSummary";
@@ -9,7 +9,7 @@ import Radios from "./form/Radios";
 import SearchSelect from "./form/SearchSelect";
 import Table from "./form/Table";
 import TextInput from "./form/TextInput";
-import TwoThirdsLayout from "./layout/Layout";
+import { TwoThirdsLayout } from "./layout/Layout";
 import { AddUserPageProps } from "../pages/admin/add-user.page";
 import { EditUserPageProps } from "../pages/admin/edit-user/[username].page";
 import { addUserSchema, AddUserSchema, OperatorData } from "../schemas/add-user.schema";
@@ -23,20 +23,40 @@ const filterConfig = {
     matchFrom: "any" as const,
 };
 
-export const userPageTemplate = (
-    title: string,
-    description: string,
-    pageState: EditUserPageProps | AddUserPageProps,
-    username: string,
-    initialGroup: string,
-    stateUpdater: (a: string) => void,
-    operatorNocCodesList: OperatorData[],
-    removeOperator: (e: SyntheticEvent, a: string) => void,
-    selectedOperator: OperatorData,
-    handleOperatorChange: () => void,
-    operatorSearchInput: string,
-    setOperatorsSearchInput: () => void,
-) => {
+interface Props {
+    pageType: "editUser" | "addUser";
+    title: string;
+    description: string;
+    pageState: EditUserPageProps | AddUserPageProps;
+    username?: string;
+    initialGroup?: string;
+    stateUpdater: (
+        a: string,
+        field: "operatorNocCodes" | "email" | "orgId" | "givenName" | "familyName" | "group",
+    ) => void;
+    operatorNocCodesList: OperatorData[];
+    removeOperator: (e: SyntheticEvent, a: string) => void;
+    selectedOperator: SingleValue<OperatorData>;
+    handleOperatorChange: (value: SingleValue<OperatorData>) => void;
+    operatorSearchInput: string;
+    setOperatorsSearchInput: Dispatch<SetStateAction<string>>;
+}
+
+const UserPageTemplate = ({
+    pageType,
+    title,
+    description,
+    pageState,
+    username,
+    initialGroup,
+    stateUpdater,
+    operatorNocCodesList,
+    removeOperator,
+    selectedOperator,
+    handleOperatorChange,
+    operatorSearchInput,
+    setOperatorsSearchInput,
+}: Props) => {
     const getOperatorRows = () => {
         if (pageState.inputs.operatorNocCodes) {
             return sortOperatorByName(pageState.inputs.operatorNocCodes).map((operator) => ({
@@ -57,116 +77,130 @@ export const userPageTemplate = (
     };
 
     return (
-        <TwoThirdsLayout title={title} description={description} errors={pageState.errors}>
-            <CsrfForm action="/api/admin/edit-user" method="post" csrfToken={pageState.csrfToken}>
-                <>
-                    <ErrorSummary errors={pageState.errors} />
-                    <h1 className="govuk-heading-xl">Edit user</h1>
-                    <TextInput<AddUserSchema>
-                        display="First name"
-                        inputName="givenName"
-                        widthClass="w"
-                        value={pageState.inputs.givenName}
-                        initialErrors={pageState.errors}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                    />
-                    <TextInput<AddUserSchema>
-                        display="Last name"
-                        inputName="familyName"
-                        widthClass="w"
-                        value={pageState.inputs.familyName}
-                        initialErrors={pageState.errors}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                    />
-                    <TextInput<AddUserSchema>
-                        display="Email address"
-                        inputName="email"
-                        widthClass="w"
-                        value={pageState.inputs.email}
-                        initialErrors={pageState.errors}
-                        schema={addUserSchema.shape.email}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                        isDisabled={true}
-                    />
+        <>
+            <TwoThirdsLayout title={title} description={description} errors={pageState.errors}>
+                <CsrfForm
+                    action={pageType === "addUser" ? "/api/admin/add-user" : "/api/admin/edit-user"}
+                    method="post"
+                    csrfToken={pageState.csrfToken}
+                >
+                    <>
+                        <ErrorSummary errors={pageState.errors} />
+                        <h1 className="govuk-heading-xl">{pageType === "addUser" ? "Add User" : "Edit user"}</h1>
+                        <TextInput<AddUserSchema>
+                            display="First name"
+                            inputName="givenName"
+                            widthClass="w"
+                            value={pageState.inputs.givenName}
+                            initialErrors={pageState.errors}
+                            stateUpdater={stateUpdater}
+                            maxLength={100}
+                        />
+                        <TextInput<AddUserSchema>
+                            display="Last name"
+                            inputName="familyName"
+                            widthClass="w"
+                            value={pageState.inputs.familyName}
+                            initialErrors={pageState.errors}
+                            stateUpdater={stateUpdater}
+                            maxLength={100}
+                        />
+                        <TextInput<AddUserSchema>
+                            display="Email address"
+                            inputName="email"
+                            widthClass="w"
+                            value={pageState.inputs.email}
+                            initialErrors={pageState.errors}
+                            schema={addUserSchema.shape.email}
+                            stateUpdater={stateUpdater}
+                            maxLength={100}
+                            isDisabled={pageType === "addUser" ? false : true}
+                        />
 
-                    <Table rows={[{ header: "Organisation", cells: [pageState.sessionWithOrg?.orgName, ""] }]} />
+                        <Table rows={[{ header: "Organisation", cells: [pageState.sessionWithOrg?.orgName, ""] }]} />
 
-                    <Radios<AddUserSchema>
-                        display="What account do they require?"
-                        radioDetail={[
-                            {
-                                value: UserGroups.orgAdmins,
-                                display: "Admin",
-                            },
-                            {
-                                value: UserGroups.orgPublishers,
-                                display: "Publishing",
-                            },
-                            {
-                                value: UserGroups.orgStaff,
-                                display: "Staff",
-                            },
-                            {
-                                value: UserGroups.operators,
-                                display: "Operator",
-                            },
-                        ]}
-                        inputName="group"
-                        stateUpdater={stateUpdater}
-                        value={pageState.inputs.group?.toString()}
-                        initialErrors={pageState.errors}
-                    />
+                        <Radios<AddUserSchema>
+                            display="What account do they require?"
+                            radioDetail={[
+                                {
+                                    value: UserGroups.orgAdmins,
+                                    display: "Admin",
+                                },
+                                {
+                                    value: UserGroups.orgPublishers,
+                                    display: "Publishing",
+                                },
+                                {
+                                    value: UserGroups.orgStaff,
+                                    display: "Staff",
+                                },
+                                {
+                                    value: UserGroups.operators,
+                                    display: "Operator",
+                                },
+                            ]}
+                            inputName="group"
+                            stateUpdater={stateUpdater}
+                            value={pageState.inputs.group?.toString()}
+                            initialErrors={pageState.errors}
+                        />
 
-                    {pageState.inputs.group === UserGroups.operators && (
-                        <div className={"ml-[8%]"}>
-                            <SearchSelect<OperatorData>
-                                selected={selectedOperator}
-                                inputName="operatorNocCodes"
-                                initialErrors={pageState.errors}
-                                placeholder="Select operator NOC codes"
-                                getOptionLabel={(operator) => `${operator.nocCode} - ${operator.operatorPublicName}`}
-                                options={sortOperatorByName(operatorNocCodesList).filter((operatorOption) =>
-                                    pageState.inputs.operatorNocCodes
-                                        ? !pageState.inputs.operatorNocCodes.find(
-                                              (selectedOperators) => selectedOperators.id === operatorOption.id,
-                                          )
-                                        : true,
-                                )}
-                                handleChange={handleOperatorChange}
-                                tableData={pageState?.inputs?.operatorNocCodes}
-                                getRows={getOperatorRows}
-                                getOptionValue={(operator: OperatorData) => operator.id.toString()}
-                                display="NOC Codes"
-                                hint=""
-                                displaySize="s"
-                                inputId="operatorNocCodes"
-                                isClearable
-                                inputValue={operatorSearchInput}
-                                filterOptions={createFilter(filterConfig)}
-                                setSearchInput={setOperatorsSearchInput}
-                            />
-                        </div>
-                    )}
+                        {pageState.inputs.group === UserGroups.operators && (
+                            <div className={"ml-[8%]"}>
+                                <SearchSelect<OperatorData>
+                                    selected={selectedOperator}
+                                    inputName="operatorNocCodes"
+                                    initialErrors={pageState.errors}
+                                    placeholder="Select operator NOC codes"
+                                    getOptionLabel={(operator) =>
+                                        `${operator.nocCode} - ${operator.operatorPublicName}`
+                                    }
+                                    options={sortOperatorByName(operatorNocCodesList).filter((operatorOption) =>
+                                        pageState.inputs.operatorNocCodes
+                                            ? !pageState.inputs.operatorNocCodes.find(
+                                                  (selectedOperators) => selectedOperators.id === operatorOption.id,
+                                              )
+                                            : true,
+                                    )}
+                                    handleChange={handleOperatorChange}
+                                    tableData={pageState.inputs.operatorNocCodes}
+                                    getRows={getOperatorRows}
+                                    getOptionValue={(operator: OperatorData) => operator.id.toString()}
+                                    display="NOC Codes"
+                                    hint=""
+                                    displaySize="s"
+                                    inputId="operatorNocCodes"
+                                    isClearable
+                                    inputValue={operatorSearchInput}
+                                    filterOptions={createFilter(filterConfig)}
+                                    setSearchInput={setOperatorsSearchInput}
+                                />
+                            </div>
+                        )}
 
-                    <input type="hidden" name={`email`} value={pageState.inputs.email} />
-                    <input type="hidden" name={`username`} value={username} />
-                    <input type="hidden" name={`initialGroup`} value={initialGroup} />
+                        {pageType === "editUser" && (
+                            <>
+                                <input type="hidden" name={`email`} value={pageState.inputs.email} />
+                                <input type="hidden" name={`username`} value={username} />
+                                <input type="hidden" name={`initialGroup`} value={initialGroup} />
+                            </>
+                        )}
 
-                    <button className="govuk-button mt-8" data-module="govuk-button">
-                        Save
-                    </button>
-                    <Link
-                        role="button"
-                        href="/admin/user-management"
-                        className="govuk-button mt-8 ml-5 govuk-button--secondary"
-                    >
-                        Cancel
-                    </Link>
-                </>
-            </CsrfForm>
-        </TwoThirdsLayout>
+                        <button className="govuk-button mt-8" data-module="govuk-button">
+                            {pageType === "addUser" ? "Send invitation" : "Save"}
+                        </button>
+                        <Link
+                            role="button"
+                            href="/admin/user-management"
+                            className="govuk-button mt-8 ml-5 govuk-button--secondary"
+                        >
+                            Cancel
+                        </Link>
+                    </>
+                </CsrfForm>
+            </TwoThirdsLayout>
+        </>
     );
 };
+
+export default UserPageTemplate;
--- site/pages/admin/edit-user/[username].page.tsx
+++ site/pages/admin/edit-user/[username].page.tsx
@@ -1,39 +1,21 @@
-import { UserGroups } from "@create-disruptions-data/shared-ts/enums";
 import { NextPageContext } from "next";
-import Link from "next/link";
 import { parseCookies } from "nookies";
 import { ReactElement, SyntheticEvent, useState } from "react";
-import { createFilter, SingleValue } from "react-select";
-import type { FilterOptionOption } from "react-select/dist/declarations/src/filters";
-import CsrfForm from "../../../components/form/CsrfForm";
-import ErrorSummary from "../../../components/form/ErrorSummary";
-import Radios from "../../../components/form/Radios";
-import SearchSelect from "../../../components/form/SearchSelect";
-import Table from "../../../components/form/Table";
-import TextInput from "../../../components/form/TextInput";
-import { TwoThirdsLayout } from "../../../components/layout/Layout";
+import { SingleValue } from "react-select";
+import UserPageTemplate from "../../../components/user-page-template";
 import { COOKIES_EDIT_USER_ERRORS } from "../../../constants";
 import { getGroupForUser, getUserDetails } from "../../../data/cognito";
 import { fetchOperators } from "../../../data/refDataApi";
 import { PageState } from "../../../interfaces";
 import { AddUserSchema, addUserSchema, OperatorData, operatorDataSchema } from "../../../schemas/add-user.schema";
-
 import { user } from "../../../schemas/user-management.schema";
-import { flattenZodErrors, sortOperatorByName } from "../../../utils";
+import { flattenZodErrors } from "../../../utils";
 import { destroyCookieOnResponseObject, getPageState } from "../../../utils/apiUtils";
 import { getSessionWithOrgDetail } from "../../../utils/apiUtils/auth";
 import { getStateUpdater } from "../../../utils/formUtils";
 
 const title = "Edit User - Create Transport Disruptions Service";
 const description = "Edit User page for the Create Transport Disruptions Service";
-
-const filterConfig = {
-    ignoreCase: true,
-    ignoreAccents: false,
-    stringify: <Option extends object>(option: FilterOptionOption<Option>) => `${option.label}`,
-    trim: true,
-    matchFrom: "any" as const,
-};
 
 export interface EditUserPageProps extends PageState<Partial<AddUserSchema>> {
     operatorData?: OperatorData[];
@@ -78,7 +60,7 @@ const EditUser = (props: EditUserPageProps): ReactElement => {
         e.preventDefault();
 
         if (pageState?.inputs?.operatorNocCodes) {
-            const updatedoperatorNocCodesArray = [...pageState.inputs.operatorNocCodes].filter(
+            const updatedOperatorNocCodesArray = [...pageState.inputs.operatorNocCodes].filter(
                 (operator) => operator.nocCode !== removedNocCode,
             );
 
@@ -86,7 +68,7 @@ const EditUser = (props: EditUserPageProps): ReactElement => {
                 ...pageState,
                 inputs: {
                     ...pageState.inputs,
-                    operatorNocCodes: updatedoperatorNocCodesArray,
+                    operatorNocCodes: updatedOperatorNocCodesArray,
                 },
                 errors: pageState.errors,
             });
@@ -94,137 +76,22 @@ const EditUser = (props: EditUserPageProps): ReactElement => {
         setSelectedOperator(null);
     };
 
-    const getOperatorRows = () => {
-        if (pageState.inputs.operatorNocCodes) {
-            return sortOperatorByName(pageState.inputs.operatorNocCodes).map((operator) => ({
-                cells: [
-                    `${operator.nocCode} - ${operator.operatorPublicName}`,
-                    <button
-                        id={`remove-service-${operator.nocCode}`}
-                        key={`remove-service-${operator.nocCode}`}
-                        className="govuk-link"
-                        onClick={(e) => Promise.resolve(removeOperator(e, operator.nocCode))}
-                    >
-                        Remove
-                    </button>,
-                ],
-            }));
-        }
-        return [];
-    };
-
     return (
-        <TwoThirdsLayout title={title} description={description} errors={pageState.errors}>
-            <CsrfForm action="/api/admin/edit-user" method="post" csrfToken={pageState.csrfToken}>
-                <>
-                    <ErrorSummary errors={pageState.errors} />
-                    <h1 className="govuk-heading-xl">Edit user</h1>
-                    <TextInput<AddUserSchema>
-                        display="First name"
-                        inputName="givenName"
-                        widthClass="w"
-                        value={pageState.inputs.givenName}
-                        initialErrors={pageState.errors}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                    />
-                    <TextInput<AddUserSchema>
-                        display="Last name"
-                        inputName="familyName"
-                        widthClass="w"
-                        value={pageState.inputs.familyName}
-                        initialErrors={pageState.errors}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                    />
-                    <TextInput<AddUserSchema>
-                        display="Email address"
-                        inputName="email"
-                        widthClass="w"
-                        value={pageState.inputs.email}
-                        initialErrors={pageState.errors}
-                        schema={addUserSchema.shape.email}
-                        stateUpdater={stateUpdater}
-                        maxLength={100}
-                        isDisabled={true}
-                    />
-
-                    <Table rows={[{ header: "Organisation", cells: [pageState.sessionWithOrg?.orgName, ""] }]} />
-
-                    <Radios<AddUserSchema>
-                        display="What account do they require?"
-                        radioDetail={[
-                            {
-                                value: UserGroups.orgAdmins,
-                                display: "Admin",
-                            },
-                            {
-                                value: UserGroups.orgPublishers,
-                                display: "Publishing",
-                            },
-                            {
-                                value: UserGroups.orgStaff,
-                                display: "Staff",
-                            },
-                            {
-                                value: UserGroups.operators,
-                                display: "Operator",
-                            },
-                        ]}
-                        inputName="group"
-                        stateUpdater={stateUpdater}
-                        value={pageState.inputs.group?.toString()}
-                        initialErrors={pageState.errors}
-                    />
-
-                    {pageState.inputs.group === UserGroups.operators && (
-                        <div className={"ml-[8%]"}>
-                            <SearchSelect<OperatorData>
-                                selected={selectedOperator}
-                                inputName="operatorNocCodes"
-                                initialErrors={pageState.errors}
-                                placeholder="Select operator NOC codes"
-                                getOptionLabel={(operator) => `${operator.nocCode} - ${operator.operatorPublicName}`}
-                                options={sortOperatorByName(operatorNocCodesList).filter((operatorOption) =>
-                                    pageState.inputs.operatorNocCodes
-                                        ? !pageState.inputs.operatorNocCodes.find(
-                                              (selectedOperators) => selectedOperators.id === operatorOption.id,
-                                          )
-                                        : true,
-                                )}
-                                handleChange={handleOperatorChange}
-                                tableData={pageState?.inputs?.operatorNocCodes}
-                                getRows={getOperatorRows}
-                                getOptionValue={(operator: OperatorData) => operator.id.toString()}
-                                display="NOC Codes"
-                                hint=""
-                                displaySize="s"
-                                inputId="operatorNocCodes"
-                                isClearable
-                                inputValue={operatorSearchInput}
-                                filterOptions={createFilter(filterConfig)}
-                                setSearchInput={setOperatorsSearchInput}
-                            />
-                        </div>
-                    )}
-
-                    <input type="hidden" name={`email`} value={pageState.inputs.email} />
-                    <input type="hidden" name={`username`} value={props.username} />
-                    <input type="hidden" name={`initialGroup`} value={props.initialGroup} />
-
-                    <button className="govuk-button mt-8" data-module="govuk-button">
-                        Save
-                    </button>
-                    <Link
-                        role="button"
-                        href="/admin/user-management"
-                        className="govuk-button mt-8 ml-5 govuk-button--secondary"
-                    >
-                        Cancel
-                    </Link>
-                </>
-            </CsrfForm>
-        </TwoThirdsLayout>
+        <UserPageTemplate
+            pageType={"editUser"}
+            title={title}
+            description={description}
+            pageState={pageState}
+            username={props.username ?? ""}
+            initialGroup={props.initialGroup ?? ""}
+            stateUpdater={stateUpdater}
+            operatorNocCodesList={operatorNocCodesList}
+            removeOperator={removeOperator}
+            selectedOperator={selectedOperator}
+            handleOperatorChange={handleOperatorChange}
+            operatorSearchInput={operatorSearchInput}
+            setOperatorsSearchInput={setOperatorsSearchInput}
+        />
     );
 };
 
