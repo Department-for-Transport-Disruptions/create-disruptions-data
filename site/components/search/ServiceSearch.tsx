748af6345c9c3e9f16c129069d699fbd66d60284
Alaina Jaffer <59929701+alainajaffer@users.noreply.github.com>
2023-12-19 06:42:24 -0800

    fix: Duplicates in dropdown from different data sources (#382)

--- site/components/search/ServiceSearch.tsx
+++ site/components/search/ServiceSearch.tsx
@@ -1,12 +1,16 @@
 import { Service } from "@create-disruptions-data/shared-ts/disruptionTypes";
+import { Datasource } from "@create-disruptions-data/shared-ts/enums";
 import { Dispatch, ReactElement, SetStateAction, useEffect, useState } from "react";
 import Select, { ControlProps, GroupBase, OptionProps } from "react-select";
 import { getServiceLabel } from "../../utils";
+import Dropdown from "../form/Select";
 
 interface ServiceSearchProps {
     services: Service[];
     setSelectedServices: Dispatch<SetStateAction<Service[]>>;
     selectedServices: Service[];
+    handleDataSourceUpdate: Dispatch<SetStateAction<string>>;
+    dataSource: string;
     reset?: boolean;
 }
 
@@ -15,6 +19,8 @@ const ServiceSearch = ({
     setSelectedServices,
     selectedServices,
     reset = false,
+    dataSource,
+    handleDataSourceUpdate,
 }: ServiceSearchProps): ReactElement => {
     const [searchText, setSearchText] = useState("");
 
@@ -40,42 +46,60 @@ const ServiceSearch = ({
     });
 
     return (
-        <div className="govuk-form-group">
-            <label className="govuk-label govuk-label--s" htmlFor="service-filter-dropdown-value">
-                Services
-            </label>
-            <Select
-                isSearchable
-                styles={{
-                    control: (baseStyles, state) => ({
-                        ...baseStyles,
-                        ...controlStyles(state),
-                    }),
-                    option: (provided, state) => ({
-                        ...provided,
-                        ...optionStyles(state),
-                    }),
-                }}
-                placeholder="Select services"
-                getOptionLabel={getServiceLabel}
-                getOptionValue={(service: Service) => service.id.toString()}
-                options={services}
-                onInputChange={(text) => {
-                    setSearchText(text);
-                }}
-                inputValue={searchText}
-                onChange={(service) => {
-                    if (!selectedServices.find((selService) => selService.id === (service as Service).id)) {
-                        setSelectedServices([...selectedServices, service as Service]);
-                    }
-                }}
-                id="service-filter"
-                instanceId="service-filter-dropdown"
-                inputId="service-filter-dropdown-value"
-                menuPlacement="auto"
-                menuPosition="fixed"
-                value={null}
-            />
+        <div className="govuk-form-group flex">
+            <div className="w-2/3">
+                <label className="govuk-label govuk-label--s" htmlFor="service-filter-dropdown-value">
+                    Services
+                </label>
+                <Select
+                    isSearchable
+                    styles={{
+                        control: (baseStyles, state) => ({
+                            ...baseStyles,
+                            ...controlStyles(state),
+                        }),
+                        option: (provided, state) => ({
+                            ...provided,
+                            ...optionStyles(state),
+                        }),
+                    }}
+                    placeholder="Select services"
+                    getOptionLabel={getServiceLabel}
+                    getOptionValue={(service: Service) => service.id.toString()}
+                    options={services}
+                    onInputChange={(text) => {
+                        setSearchText(text);
+                    }}
+                    inputValue={searchText}
+                    onChange={(service) => {
+                        if (!selectedServices.find((selService) => selService.id === (service as Service).id)) {
+                            setSelectedServices([...selectedServices, service as Service]);
+                        }
+                    }}
+                    id="service-filter"
+                    instanceId="service-filter-dropdown"
+                    inputId="service-filter-dropdown-value"
+                    menuPlacement="auto"
+                    menuPosition="fixed"
+                    value={null}
+                />
+            </div>
+            <div className="w-1/3">
+                <Dropdown
+                    inputName="servicesDataSource"
+                    display="Services data source"
+                    value={dataSource || "all"}
+                    defaultDisplay="Select a services data source"
+                    selectValues={[
+                        { display: "All sources", value: "all" },
+                        { display: "BODS", value: Datasource.bods },
+                        { display: "TNDS", value: Datasource.tnds },
+                    ]}
+                    stateUpdater={handleDataSourceUpdate}
+                    width="1/4"
+                    useDefaultValue={false}
+                />
+            </div>
         </div>
     );
 };
