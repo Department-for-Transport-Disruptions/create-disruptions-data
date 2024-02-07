d03b0196ef3de4b47ebf9e60f04839f71bd28ce5
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2024-02-07 11:52:54 +0000

    fix: Remove all stops/services buttons appear before stops/services are selected

--- site/pages/create-consequence-stops/[disruptionId]/[consequenceIndex].page.tsx
+++ site/pages/create-consequence-stops/[disruptionId]/[consequenceIndex].page.tsx
@@ -265,26 +265,28 @@ const CreateConsequenceStops = (props: CreateConsequenceStopsProps): ReactElemen
                             }}
                         />
 
-                        <div className="my-3">
-                            <button
-                                className="govuk-link"
-                                data-module="govuk-button"
-                                onClick={(e) => {
-                                    e.preventDefault();
-                                    setPageState({
-                                        ...pageState,
-                                        inputs: {
-                                            ...pageState.inputs,
-                                            stops: [],
-                                        },
-                                        errors: pageState.errors,
-                                    });
-                                }}
-                                disabled={!pageState.inputs.stops || pageState.inputs.stops?.length === 0}
-                            >
-                                <p className="text-govBlue govuk-body-m">Remove all stops</p>
-                            </button>
-                        </div>
+                        {pageState.inputs.stops && pageState.inputs.stops?.length >= 1 && (
+                            <div className="my-3">
+                                <button
+                                    className="govuk-link"
+                                    data-module="govuk-button"
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        setPageState({
+                                            ...pageState,
+                                            inputs: {
+                                                ...pageState.inputs,
+                                                stops: [],
+                                            },
+                                            errors: pageState.errors,
+                                        });
+                                    }}
+                                    disabled={!pageState.inputs.stops || pageState.inputs.stops?.length === 0}
+                                >
+                                    <p className="text-govBlue govuk-body-m">Remove all stops</p>
+                                </button>
+                            </div>
+                        )}
                         <br />
 
                         <Map
