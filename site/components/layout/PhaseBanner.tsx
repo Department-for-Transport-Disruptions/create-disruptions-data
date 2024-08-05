046e027b2eab735be812373dafcae9e88aabba75
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2024-08-05 15:09:44 +0100

    fix header and phase banner

--- site/components/layout/PhaseBanner.tsx
+++ site/components/layout/PhaseBanner.tsx
@@ -5,7 +5,7 @@ const PhaseBanner = (): ReactElement => (
     <div className="app-phase-banner__wrapper">
         <div className="govuk-phase-banner max-w-[960px] w-[90%] border-0 mx-auto app-phase-banner app-width-container">
             <p className="govuk-phase-banner__content">
-                <strong className="govuk-tag govuk-phase-banner__content__tag">beta</strong>
+                <strong className="govuk-tag govuk-phase-banner__content__tag">Beta</strong>
                 <span className="govuk-phase-banner__text">
                     This is a new service – your{" "}
                     <Link className="govuk-link" id="feedback-link" href="/feedback">
