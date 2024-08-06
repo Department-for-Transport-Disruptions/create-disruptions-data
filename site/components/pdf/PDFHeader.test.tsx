3f9b9bab6f6c31ea555b9460bfa35b062623837b
ajaffer1 <alaina.jaffer@kpmg.co.uk>
2024-08-06 18:50:37 +0100

    fix lint

--- site/components/pdf/PDFHeader.test.tsx
+++ site/components/pdf/PDFHeader.test.tsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-import PDFHeader from './PDFHeader';
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import PDFHeader from "./PDFHeader";
 
-describe('PDFHeader', () => {
-    it('should render correctly', () => {
+describe("PDFHeader", () => {
+    it("should render correctly", () => {
         const { asFragment } = render(<PDFHeader />);
         expect(asFragment()).toMatchSnapshot();
     });
--- site/components/pdf/PDFRows.test.tsx
+++ site/components/pdf/PDFRows.test.tsx
@@ -1,10 +1,10 @@
-import { render } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
-import { exportDisruption } from '../../testData/mockData';
-import PDFRows from './PDFRows';
+import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { exportDisruption } from "../../testData/mockData";
+import PDFRows from "./PDFRows";
 
-describe('PDFRows', () => {
-    it('should render correctly', () => {
+describe("PDFRows", () => {
+    it("should render correctly", () => {
         const { asFragment } = render(<PDFRows disruptions={exportDisruption} />);
         expect(asFragment()).toMatchSnapshot();
     });
