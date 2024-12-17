4cb9e75c45b7275b415c503a8c0f89b87ce315b3
Deanna Sharma <deanna.sharma@doza.consulting>
2024-12-17 09:34:27 +0000

    feat: add index for publish status

---
+++ shared-ts/db/migrations/1734427558_publish_status_index.mjs
@@ -0,0 +1,15 @@
+import { Kysely } from "kysely";
+
+/**
+ *
+ * @param {Kysely} db
+ */
+export async function up(db) {
+    await db.schema.createIndex("idx_disruptions_publish_status").on("disruptions").column("publish_status").execute();
+
+    await db.schema
+        .createIndex("idx_disruptions_edited_publish_status")
+        .on("disruptions_edited")
+        .column("publish_status")
+        .execute();
+}
