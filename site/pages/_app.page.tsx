3431ba5dc3d4dc2d95ce2e3053055f19bf901256
Deanna Sharma <deanna.sharma@doza.consulting>
2025-05-06 16:09:47 +0100

    update to govuk-frontend 5.10.0

--- site/pages/_app.page.tsx
+++ site/pages/_app.page.tsx
@@ -2,12 +2,11 @@ import "../styles/globals.scss";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppContext, AppInitialProps, AppProps } from "next/app";
 import App from "next/app";
-import { useEffect, useRef } from "react";
+import {} from "react";
 import Header from "../components/layout/Header";
 import type { Session } from "../schemas/session.schema";
 import { getCsrfToken } from "../utils";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { initAll } from "govuk-frontend";
 
 config.autoAddCss = false;
 
@@ -25,23 +24,6 @@ type ExtendedAppProps = {
 };
 
 const CustomApp = ({ Component, pageProps, csrfToken, session }: AppProps & ExtendedAppProps) => {
-    const initialized = useRef(false);
-
-    useEffect(() => {
-        if (!initialized.current) {
-            initialized.current = true;
-
-            const bodyElement = document.getElementsByTagName("body")[0];
-            bodyElement.classList.add("js-enabled");
-
-            if ("noModule" in HTMLScriptElement.prototype) {
-                bodyElement.classList.add("govuk-frontend-supported");
-            }
-
-            initAll();
-        }
-    }, []);
-
     return (
         <>
             <Header session={session} csrfToken={csrfToken} />
