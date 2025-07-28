7b06848699ce6c71cd92feaafd31486cf19ed612
Deanna Sharma <deanna.sharma@doza.consulting>
2025-07-28 11:45:09 +0100

    chore: add cli helpers for migrating roadworks data

---
+++ cli-helpers/src/commands/download-ref-data-roadworks.ts
@@ -0,0 +1,88 @@
+import { program } from "commander";
+import { DataApiDialect } from "kysely-data-api";
+import { RDSData } from "@aws-sdk/client-rds-data";
+import { Database } from "@create-disruptions-data/shared-ts/db/types";
+import { Kysely } from "kysely";
+import path from "node:path";
+import * as fs from "node:fs";
+import { withUserPrompt } from "../utils";
+
+program
+    .option("--refDataDatabaseName <refDataDatabaseName>", "Name of reference data database")
+    .option("--refDataDbSecretArn <refDataDbSecretArn>", "ARN of reference data DB secret")
+    .option("--refDataDbResourceArn <refDataDbResourceArn>", "ARN of reference data database")
+    .action(async () => {
+        let { refDataDatabaseName, refDataDbSecretArn, refDataDbResourceArn } = program.opts();
+
+        if (!refDataDatabaseName) {
+            refDataDatabaseName = await withUserPrompt("refDataDatabaseName", { type: "input" });
+        }
+
+        if (!refDataDbSecretArn) {
+            refDataDbSecretArn = await withUserPrompt("refDataDbSecretArn", { type: "input" });
+        }
+
+        if (!refDataDbResourceArn) {
+            refDataDbResourceArn = await withUserPrompt("refDataDbResourceArn", { type: "input" });
+        }
+
+        if (refDataDbResourceArn.includes("prod") && !refDataDbResourceArn.includes("preprod")) {
+            const prodRunConfirmed = await withUserPrompt("confirmProdRun", {
+                type: "confirm",
+                message:
+                    "You are going to run this command against the preprod database. Are you sure you want to continue?",
+                default: false,
+            });
+
+            if (!prodRunConfirmed) {
+                console.log("Script aborted. Exiting without making changes.");
+                process.exit(0);
+            }
+        }
+
+        const refDataDbClient = new Kysely<Database>({
+            dialect: new DataApiDialect({
+                mode: "mysql",
+                driver: {
+                    database: refDataDatabaseName,
+                    secretArn: refDataDbSecretArn,
+                    resourceArn: refDataDbResourceArn,
+                    client: new RDSData({
+                        region: "eu-west-2",
+                    }),
+                },
+            }),
+        });
+
+        const filePath = path.join(__dirname, "roadworks.json");
+        fs.writeFileSync(filePath, "[", "utf-8");
+
+        let hasMoreData = true;
+        let offset = 0;
+        const limit = 800;
+
+        while (hasMoreData) {
+            // biome-ignore lint/style/noUnusedTemplateLiteral: <explanation>
+            console.log(`Fetching roadworks data...`);
+            const roadworks = await refDataDbClient
+                .selectFrom("roadworks")
+                .selectAll()
+                .limit(limit)
+                .offset(offset)
+                .execute();
+
+            if (roadworks.length > 0) {
+                const jsonData = JSON.stringify(roadworks, null, 2);
+                fs.appendFileSync(filePath, (offset > 0 ? "," : "") + jsonData.slice(1, -1), "utf-8");
+                offset += limit;
+            } else {
+                hasMoreData = false;
+            }
+        }
+
+        fs.appendFileSync(filePath, "]", "utf-8");
+
+        console.log(`Roadworks data saved to ${filePath}`);
+    });
+
+program.parseAsync(process.argv);
---
+++ cli-helpers/src/commands/upload-roadworks.ts
@@ -0,0 +1,105 @@
+import { program } from "commander";
+import { Database, NewRoadworkDB } from "@create-disruptions-data/shared-ts/db/types";
+import { CamelCasePlugin, Kysely, PostgresDialect } from "kysely";
+import pg from "pg";
+import * as fs from "node:fs";
+import path from "node:path";
+import { roadworkSchema } from "@create-disruptions-data/shared-ts/roadwork.zod";
+import { z } from "zod";
+import { Promise as BluebirdPromise } from "bluebird";
+import { withUserPrompt } from "../utils";
+
+const { Pool } = pg;
+
+export const chunkArray = (array: NewRoadworkDB[], chunkSize: number) => {
+    const chunks: NewRoadworkDB[][] = [];
+
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+    }
+
+    return chunks;
+};
+
+program
+    .option("--disruptionsDbUsername <disruptionsDbUsername>", "Username for disruptions database")
+    .option("--disruptionsDbPassword <disruptionsDbPassword>", "Password for disruptions database")
+    .option("--disruptionsDbHostName <disruptionsDbHostName>", "Hostname for disruptions database")
+    .option("--disruptionsLocalPort <disruptionsLocalPort>", "Local port for disruptions database")
+    .option("--disruptionsDatabaseName <disruptionsDatabaseName>", "Name of the disruptions database")
+    .action(async () => {
+        let {
+            disruptionsDbUsername,
+            disruptionsDbPassword,
+            disruptionsDbHostName,
+            disruptionsLocalPort,
+            disruptionsDatabaseName,
+        } = program.opts();
+
+        if (!disruptionsDbUsername) {
+            disruptionsDbUsername = await withUserPrompt("disruptionsDbUsername", { type: "input" });
+        }
+
+        if (!disruptionsDbPassword) {
+            disruptionsDbPassword = await withUserPrompt("disruptionsDbPassword", { type: "password" });
+        }
+
+        if (!disruptionsDbHostName) {
+            disruptionsDbHostName = await withUserPrompt("disruptionsDbHostName", { type: "input" });
+        }
+
+        if (!disruptionsLocalPort) {
+            disruptionsLocalPort = await withUserPrompt("disruptionsLocalPort", { type: "input" });
+        }
+
+        if (!disruptionsDatabaseName) {
+            disruptionsDatabaseName = await withUserPrompt("disruptionsDatabaseName", { type: "input" });
+        }
+
+        const disruptionsDbClient = new Kysely<Database>({
+            dialect: new PostgresDialect({
+                pool: new Pool({
+                    connectionString: `postgresql://${encodeURIComponent(disruptionsDbUsername)}:${encodeURIComponent(disruptionsDbPassword)}@${disruptionsDbHostName}:${disruptionsLocalPort}/${disruptionsDatabaseName}`,
+                }),
+            }),
+            plugins: [new CamelCasePlugin()],
+        });
+
+        const filePath = path.join(__dirname, "roadworks.json");
+        const roadworksData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        const processedRoadworks = roadworksData.map((roadwork: any) => ({
+            ...roadwork,
+            lastUpdatedDateTime: roadwork.lastUpdatedDatetime,
+        }));
+
+        const parsedRoadworks = z
+            .array(roadworkSchema.extend({ createdDateTime: z.string().datetime() }))
+            .safeParse(processedRoadworks);
+
+        if (!parsedRoadworks.success) {
+            console.error(`Error parsing roadworks data: ${processedRoadworks.error}`);
+            process.exit(0);
+        }
+
+        const writeToRoadworksTable = async (batch: NewRoadworkDB[]) => {
+            console.log("Inserting roadworks into disruptions database...");
+            await disruptionsDbClient
+                .insertInto("roadworks")
+                .values(batch)
+                .onConflict((oc) => oc.doNothing())
+                .execute()
+                .then(() => 0);
+        };
+
+        const roadworksChunks = chunkArray(parsedRoadworks.data, 50);
+
+        await BluebirdPromise.map(roadworksChunks, (batch) => writeToRoadworksTable(batch), {
+            concurrency: 10,
+        });
+
+        console.log("Successfully uploaded roadworks to the disruptions database.");
+    });
+
+program.parseAsync(process.argv);
