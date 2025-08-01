0f5e963725041a6fe162164f5ca361fcedb6ca60
Deanna Sharma <83244002+DeannaSharma@users.noreply.github.com>
2025-08-01 14:02:11 +0100

    fix: Add roadworks queue function to vpc (#581)

--- stacks/QueueStack.ts
+++ stacks/QueueStack.ts
@@ -1,8 +1,11 @@
+import { SubnetType } from "aws-cdk-lib/aws-ec2";
 import { Queue, StackContext, use } from "sst/constructs";
 import { RdsStack } from "./RdsStack";
+import { VpcStack } from "./VpcStack";
 
 export function QueueStack({ stack }: StackContext) {
     const { dbUsernameSecret, dbPasswordSecret, dbNameSecret, dbHostSecret, dbPortSecret } = use(RdsStack);
+    const { vpc, lambdaSg } = use(VpcStack);
 
     const streetManagerSqsQueue = new Queue(stack, "ref-data-service-street-manager-queue", {
         consumer: {
@@ -12,6 +15,14 @@ export function QueueStack({ stack }: StackContext) {
                 timeout: 10,
                 runtime: "nodejs20.x",
                 logRetention: stack.stage === "prod" ? "one_month" : "two_weeks",
+                vpc,
+                vpcSubnets: {
+                    subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+                },
+                securityGroups: [lambdaSg],
+                nodejs: {
+                    install: ["pg", "kysely"],
+                },
             },
             cdk: {
                 eventSource: {
